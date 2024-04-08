a2f8d1be594e0e2cf88e77d48324288ba92f8ad7
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-04-08 05:36:56 -0500

    💭

--- swarmauri/__init__.py
+++ swarmauri/__init__.py
@@ -1,2 +1,21 @@
 __version__ = "0.1.19"
-__long_desc__ = open('./swarmauri/README.md').read()
+__long_desc__ = """
+# swarmaURI sdk
+
+This repository includes core interfaces, standard ABCs, and standard concrete references of the SwarmaURI Framework.
+
+
+## Core
+- ABCs
+- Utilities
+
+## Standard
+- Concrete Classes
+
+## Community
+- Concrete Classes that utilize third party plug-ins
+
+## Experimental
+- Tools in development
+
+"""
