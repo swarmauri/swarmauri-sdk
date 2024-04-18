40ca24d202fb5886458ada5b84bd4af2af148050
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-04-18 12:48:15 -0500

    🔨 v0.1.71 - IVectorStore not found

--- swarmauri/__init__.py
+++ swarmauri/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.71"
+__version__ = "0.1.72"
 __long_desc__ = """
 # swarmaURI sdk
 
--- swarmauri/core/agents/IAgentVectorStore.py
+++ swarmauri/core/agents/IAgentVectorStore.py
@@ -1,14 +1,13 @@
 from abc import ABC, abstractmethod
-from swarmauri.core.vector_stores.IVectorStore import IVectorStore
 
 class IAgentVectorStore(ABC):
     
     @property
     @abstractmethod
-    def vector_store(self) -> IVectorStore:
+    def vector_store(self):
         pass
 
     @vector_store.setter
     @abstractmethod
-    def vector_store(self) -> IVectorStore:
+    def vector_store(self):
         pass
