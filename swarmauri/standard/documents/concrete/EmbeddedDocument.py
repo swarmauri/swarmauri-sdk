1ed7ff11b5f915a13c2dc44867e1a04fc37d59fb
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-04-18 07:14:11 -0500

    🤦 Fix SyntaxError

--- swarmauri/standard/documents/concrete/EmbeddedDocument.py
+++ swarmauri/standard/documents/concrete/EmbeddedDocument.py
@@ -4,4 +4,4 @@ from swarmauri.standard.documents.base.EmbeddedBase import EmbeddedBase
 
 class EmbeddedDocument(EmbeddedBase):
     def __init__(self, id,  content, metadata, embedding: Optional[IVector] = None):
-        super()(id=id, content=content, metadata=metadata, embedding=embedding)
+        EmbeddedBase.__init__(self, id=id, content=content, metadata=metadata, embedding=embedding)
