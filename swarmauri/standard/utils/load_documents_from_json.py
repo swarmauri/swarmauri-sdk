da1785b5d2f2fba1bf033b5e127f383265ee52ad
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-04-26 16:12:26 -0500

    ☀️ - load json from file or from memory

--- swarmauri/standard/utils/load_documents_from_json.py
+++ swarmauri/standard/utils/load_documents_from_json.py
@@ -2,9 +2,15 @@ import json
 from typing import List
 from swarmauri.standard.documents.concrete.Document import Document
 
-def load_documents_from_json(json_file_path):
+def load_documents_from_json_file(json_file_path):
     documents = []
     with open(json_file_path, 'r') as f:
         data = json.load(f)
+    documents = [Document(id=str(_), content=doc['content'], metadata={"document_name": doc['document_name']}) for _, doc in enumerate(data) if doc['content']]
+    return documents
+
+def load_documents_from_json(json):
+    documents = []
+    data = json.loads(f)
     documents = [Document(id=str(_), content=doc['content'], metadata={"document_name": doc['document_name']}) for _, doc in enumerate(data) if doc['content']]
     return documents
