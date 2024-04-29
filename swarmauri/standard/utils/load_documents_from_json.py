2be4fd44d3e76b00ffff16f5a0c2fef76821ed54
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-04-29 03:46:01 -0500

    🧹🧹🧹

--- swarmauri/standard/utils/load_documents_from_json.py
+++ swarmauri/standard/utils/load_documents_from_json.py
@@ -6,12 +6,23 @@ def load_documents_from_json_file(json_file_path):
     documents = []
     with open(json_file_path, 'r') as f:
         data = json.load(f)
-    documents = [Document(id=str(_), content=doc['content'], metadata={"document_name": doc['document_name']}) for _, doc in enumerate(data) if doc['content']]
+
+    documents = [
+        Document(id=str(_), 
+        content=doc['content'], 
+        metadata={"document_name": doc['document_name']}) 
+        for _, doc in enumerate(data) if doc['content']
+        ]
+
     return documents
 
 def load_documents_from_json(json):
     documents = []
     data = json.loads(json)
-    documents = [Document(id=str(_), content=doc['content'], metadata={"document_name": doc['document_name']}) for _, doc in enumerate(data) if doc['content']]
+    documents = [
+        Document(id=str(_), 
+        content=doc['content'], 
+        metadata={"document_name": doc['document_name']}) 
+        for _, doc in enumerate(data) if doc['content']
+        ]
     return documents
-    
