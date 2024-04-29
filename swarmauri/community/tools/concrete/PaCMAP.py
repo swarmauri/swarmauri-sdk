16ce372d0258ceaa294e6b01f47a990d58d9dcfa
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-04-29 03:59:27 -0500

    📛 Fix NameError - F821

--- swarmauri/community/tools/concrete/PaCMAP.py
+++ swarmauri/community/tools/concrete/PaCMAP.py
@@ -2,7 +2,7 @@ import numpy as np
 import pacmap  # Ensure pacmap is installed
 from swarmauri.standard.tools.base.ToolBase import ToolBase
 from swarmauri.standard.tools.concrete.Parameter import Parameter
-class PaCMAPTool(ITool):
+class PaCMAPTool(ToolBase):
     """
     A tool for applying the PaCMAP method for dimensionality reduction.
     """
