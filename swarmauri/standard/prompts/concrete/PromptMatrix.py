bcb71679c1f3506ccdeb39c827e921632edd72ce
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-05-14 20:33:21 -0500

    🚀 v0.1.124.dev9

--- swarmauri/standard/prompts/concrete/PromptMatrix.py
+++ swarmauri/standard/prompts/concrete/PromptMatrix.py
@@ -1,4 +1,5 @@
 # swarmauri/standard/prompts/concrete/PromptMatrix.py
+from typing import List
 from swarmauri.standard.prompts.base.BasePromptMatrix import BasePromptMatrix
 
 class PromptMatrix(BasePromptMatrix):
