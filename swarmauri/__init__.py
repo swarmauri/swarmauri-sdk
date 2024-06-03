a44fc08cb43e4e7869a511d13ebd8aad1e226c77
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-03 10:31:59 -0500

    ☀️ v0.3.0.dev20 - enhance Groq Test case

--- swarmauri/__init__.py
+++ swarmauri/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.0.dev19"
+__version__ = "0.3.0.dev20"
 __long_desc__ = """
 # swarmaURI sdk
 
--- tests/models/GroqModel_test.py
+++ tests/models/GroqModel_test.py
@@ -1,16 +1,11 @@
 import os
 from swarmauri.standard.models.concrete.GroqModel import GroqModel
 from swarmauri.standard.conversations.concrete.SimpleConversation import SimpleConversation
+from swarmauri.standard.messages.concrete.HumanMessage import HumanMessage
 
 def test_initialization():
     def test():
         GROQ_API_KEY = os.getenv('GROQ_API_KEY')
-        conversation = SimpleConversation()
-
-
-        human_message = HumanMessage(input_data)
-        conversation.add_message(human_message)
-
         model = GroqModel(api_key = GROQ_API_KEY)
         assert model.model_name == 'mixtral-8x7b-32768'
     test()
@@ -21,6 +16,7 @@ def test_call():
         conversation = SimpleConversation()
 
 
+        input_data = "Hello"
         human_message = HumanMessage(input_data)
         conversation.add_message(human_message)
 
