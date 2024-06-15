9c91adbe583b80ded7780561f901a31d35e552e0
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-15 03:22:02 -0500

    🦋 v0.3.3.dev42 - top_k_test + load_from_json_test

--- tests/vector_stores/Doc2VecVectorStore_test.py
+++ tests/vector_stores/Doc2VecVectorStore_test.py
@@ -3,14 +3,14 @@ from swarmauri.standard.documents.concrete.Document import Document
 from swarmauri.standard.vector_stores.concrete.Doc2VecVectorStore import Doc2VecVectorStore
 
 @pytest.mark.unit
-def test_1():
+def ubc_initialization_test():
 	def test():
 		vs = Doc2VecVectorStore()
 		assert vs.embedder.resource == 'Embedding'
 	test()
 
 @pytest.mark.unit
-def test_2():
+def top_k_test():
 	def test():
 		vs = Doc2VecVectorStore()
 		documents = [Document(content="test"),
@@ -20,4 +20,19 @@ def test_2():
 
 		vs.add_documents(documents)
 		assert len(vs.retrieve(query='test', top_k=2)) == 2
+	test()
+	
+@pytest.mark.unit
+def load_from_json_test():
+	def test():
+		vs = Doc2VecVectorStore()
+		documents = [Document(content="test"),
+             Document(content='test1'),
+             Document(content='test2'),
+             Document(content='test3')]
+
+		vs.add_documents(documents)
+		vs_2 = Doc2VecVectorStore.parse_raw(vs.json())
+		assert vs.id == vs_2.id
+		assert vs.document_count() == vs_2.document_count()
 	test()
--- tests/vector_stores/TfidfVectorStore_test.py
+++ tests/vector_stores/TfidfVectorStore_test.py
@@ -3,14 +3,15 @@ from swarmauri.standard.documents.concrete.Document import Document
 from swarmauri.standard.vector_stores.concrete.TfidfVectorStore import TfidfVectorStore
 
 @pytest.mark.unit
-def test_1():
+def ubc_initialization_test():
 	def test():
 		vs = TfidfVectorStore()
 		assert vs.embedder.resource == 'Embedding'
 	test()
 
+
 @pytest.mark.unit
-def test_2():
+def top_k_test():
 	def test():
 		vs = TfidfVectorStore()
 		documents = [Document(content="test"),
@@ -20,4 +21,19 @@ def test_2():
 
 		vs.add_documents(documents)
 		assert len(vs.retrieve(query='test', top_k=2)) == 2
+	test()
+
+@pytest.mark.unit
+def load_from_json_test():
+	def test():
+		vs = TfidfVectorStore()
+		documents = [Document(content="test"),
+             Document(content='test1'),
+             Document(content='test2'),
+             Document(content='test3')]
+
+		vs.add_documents(documents)
+		vs_2 = TfidfVectorStore.parse_raw(vs.json())
+		assert vs.id == vs_2.id
+		assert vs.document_count() == vs_2.document_count()
 	test()
