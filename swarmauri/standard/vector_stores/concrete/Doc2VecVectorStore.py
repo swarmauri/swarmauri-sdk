f72903f84e4e23fc4cf9efe2c47e3f7d880d218b
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-15 01:15:22 -0500

    upd

--- swarmauri/standard/vector_stores/concrete/Doc2VecVectorStore.py
+++ swarmauri/standard/vector_stores/concrete/Doc2VecVectorStore.py
@@ -11,19 +11,21 @@ from swarmauri.standard.vector_stores.base.VectorStoreSaveLoadMixin import Vecto
 
 
 class Doc2VecVectorStore(VectorStoreSaveLoadMixin, VectorStoreRetrieveMixin, VectorStoreBase):
-    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._embedding = Doc2VecEmbedding()
+        self._embedder = TfidfEmbedding()
         self._distance = CosineDistance()
+      
 
     def add_document(self, document: Document) -> None:
         self.documents.append(document)
-        self._recalculate_embeddings()
+        # Recalculate TF-IDF matrix for the current set of documents
+        self._embedder.fit([doc.content for doc in self.documents])
 
     def add_documents(self, documents: List[Document]) -> None:
         self.documents.extend(documents)
-        self._recalculate_embeddings()
+        # Recalculate TF-IDF matrix for the current set of documents
+        self._embedder.fit([doc.content for doc in self.documents])
 
     def get_document(self, id: str) -> Union[Document, None]:
         for document in self.documents:
@@ -36,35 +38,27 @@ class Doc2VecVectorStore(VectorStoreSaveLoadMixin, VectorStoreRetrieveMixin, Vec
 
     def delete_document(self, id: str) -> None:
         self.documents = [doc for doc in self.documents if doc.id != id]
-        self._recalculate_embeddings()
+        # Recalculate TF-IDF matrix for the current set of documents
+        self._embedder.fit([doc.content for doc in self.documents])
 
     def update_document(self, id: str, updated_document: Document) -> None:
         for i, document in enumerate(self.documents):
             if document.id == id:
                 self.documents[i] = updated_document
                 break
-        self._recalculate_embeddings()
 
-    def _recalculate_embeddings(self):
-        # Recalculate document embeddings for the current set of documents
-        documents_text = [_d.content for _d in self.documents if _d.content]
-        embeddings = self._embedding.fit_transform(documents_text)
-
-        embedded_documents = [Document(id=_d.id, 
-            content=_d.content, 
-            metadata=_d.metadata, 
-            embedding=embeddings[_count]) for _count, _d in enumerate(self.documents)
-            if _d.content]
-
-        self.documents = embedded_documents
+        # Recalculate TF-IDF matrix for the current set of documents
+        self._embedding.fit([doc.content for doc in self.documents])
 
     def retrieve(self, query: str, top_k: int = 5) -> List[Document]:
-        query_vector = self._vectorizer.infer_vector(query)
-        document_vectors = [_d.embedding for _d in self.documents if _d.content]
+        documents = [query]
+        documents.extend([doc.content for doc in self.documents])
+        transform_matrix = self._embedder.fit_transform(documents)
 
-        distances = self._distance.distances(query_vector, document_vectors)
+        # The inferred vector is the last vector in the transformed_matrix
+        # The rest of the matrix is what we are comparing
+        distances = self._distance.distances(transform_matrix[-1], transform_matrix[:-1])  
 
-        # Get the indices of the top_k least distant (most similar) documents
+        # Get the indices of the top_k most similar (least distant) documents
         top_k_indices = sorted(range(len(distances)), key=lambda i: distances[i])[:top_k]
-        
         return [self.documents[i] for i in top_k_indices]
