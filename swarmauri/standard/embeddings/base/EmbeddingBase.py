1e5b0506871235d5ced545b169e3cc0d57b8c5dc
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-14 21:36:04 -0500

    🦋 v0.3.3.dev32

--- swarmauri/standard/embeddings/base/EmbeddingBase.py
+++ swarmauri/standard/embeddings/base/EmbeddingBase.py
@@ -1,10 +1,9 @@
-from typing import List, Union, Any
-from swarmauri.standard.vectors.concrete.Vector import Vector
-
+from typing import Optional
+from pydantic import Field
 from swarmauri.core.ComponentBase import ComponentBase, ResourceTypes
 from swarmauri.core.embeddings.IVectorize import IVectorize
 from swarmauri.core.embeddings.IFeature import IFeature
 from swarmauri.core.embeddings.ISaveModel import ISaveModel
 
 class EmbeddingBase(IVectorize, IFeature, ISaveModel, ComponentBase):
-    resource: Optional[str] =  Field(default=ResourceTypes.DOCUMENT.value)
+    resource: Optional[str] =  Field(default=ResourceTypes.EMBEDDING.value, frozen=True)
