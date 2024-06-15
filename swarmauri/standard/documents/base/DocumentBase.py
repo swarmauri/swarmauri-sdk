f45efa01459e42fa4fa9a11318d33ed44ffa5574
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-15 03:21:40 -0500

    🦋 v0.3.3.dev42 - update typing

--- swarmauri/standard/documents/base/DocumentBase.py
+++ swarmauri/standard/documents/base/DocumentBase.py
@@ -1,13 +1,13 @@
 from abc import ABC
-from typing import Dict
+from typing import Dict, Optional
 from pydantic import Field, ConfigDict
 from swarmauri.core.ComponentBase import ComponentBase, ResourceTypes
 from swarmauri.core.documents.IDocument import IDocument
-from swarmauri.core.vectors.IVector import IVector
+from swarmauri.standard.vectors.concrete.Vector import Vector
 
 class DocumentBase(IDocument, ComponentBase, ABC):
     content: str
     metadata: Dict = {}
-    embedding: IVector = None
+    embedding: Optional[Vector] = None
     model_config = ConfigDict(extra='forbid', arbitrary_types_allowed=True)
-    resource: Optional[str] =  Field(default=ResourceTypes.DOCUMENT.value)
+    resource: Optional[str] =  Field(default=ResourceTypes.DOCUMENT.value, frozen=True)
