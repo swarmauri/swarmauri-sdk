ed52149c1f19e91edf6478ae8a5ac09fb6487d29
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-17 03:13:26 -0500

    v0.3.3.dev59

--- swarmauri/standard/agents/base/AgentToolMixin.py
+++ swarmauri/standard/agents/base/AgentToolMixin.py
@@ -1,8 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from swarmauri.core.agents.IAgentToolkit import IAgentToolkit
 from swarmauri.core.toolkits.IToolkit import IToolkit
 
 
 class AgentToolMixin(IAgentToolkit, BaseModel):
     toolkit: IToolkit
+    model_config = ConfigDict(extra='forbid', arbitrary_types_allowed=True)
     
