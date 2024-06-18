dc31ff58182c5fa0c562259332a55b31bbe7393e
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-18 02:19:15 -0500

    🔨v0.3.3.dev75 - addresses validation error detected on test_method_call()

--- swarmauri/standard/chains/base/ChainStepBase.py
+++ swarmauri/standard/chains/base/ChainStepBase.py
@@ -9,7 +9,7 @@ class ChainStepBase(IChainStep, ComponentBase):
     """
     key: str = Field(kw_only=True)
     method: Callable = Field(kw_only=True)
-    args: Tuple[Any] = Field(kw_only=True, default_factory=tuple)
+    args: Tuple = Field(kw_only=True, default_factory=tuple)
     kwargs: Dict[str, Any] = Field(kw_only=True, default_factory=dict)
     ref: Optional[str] =  Field(kw_only=True, default=None)
     resource: Optional[str] =  Field(default=ResourceTypes.CHAINSTEP.value)
