d31cd96dde9396e08e52f908a9304af7379372bb
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-18 14:58:42 -0500

    🐛v0.3.3.dev90 - add _format_messages(self, messages)
    add _format_messages(self, messages)

--- swarmauri/standard/llms/concrete/CohereModel.py
+++ swarmauri/standard/llms/concrete/CohereModel.py
@@ -11,9 +11,29 @@ class CohereModel(LLMBase):
     'command-r-plus']
     name: str = "command-light"
     
+    def _format_messages(self, messages):
+        """
+        Cohere utilizes the following roles: CHATBOT, SYSTEM, TOOL, USER
+        """
+        message_properties = ['content', 'role']
+
+        messages = [message.dict(include=message_properties) for message in messages]
+        for message in messages:
+            message['message'] = message.pop('content')
+            if message.get('role') == 'assistant':
+                message['role'] = 'chatbot'
+            message['role'] = message['role'].upper()
+        return messages
+
+
     def predict(self, messages, temperature=0.7, max_tokens=256):
-        message = messages[0].content
-        messages = messages[0:]
+        # Get next message
+        message = messages[-1].content
+
+        # Format chat_history
+        messages = _format_messages(messages[:-1])
+
+
         client = cohere.Client(api_key=self.api_key)
         response = client.chat(
             model=self.name,
