dd9065c76b9cd246d52930a8f919892a9b8927ba
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-18 08:22:28 -0500

    🐛 v0.3.3.dev83 - debugging
    attempting to debug GroqModel

--- swarmauri/standard/messages/concrete/HumanMessage.py
+++ swarmauri/standard/messages/concrete/HumanMessage.py
@@ -3,4 +3,5 @@ from swarmauri.standard.messages.base.MessageBase import MessageBase
 
 class HumanMessage(MessageBase):
     content: str
+    name: str = Field(default='user')
     role: str = Field(default='user')
