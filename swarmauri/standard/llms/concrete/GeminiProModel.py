ea2c647680cff4c848a4c107e2df0a356f6afcb1
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-18 16:36:29 -0500

    🦋 v0.3.3.dev95 - add _format_messages & _get_system_context

--- swarmauri/standard/llms/concrete/GeminiProModel.py
+++ swarmauri/standard/llms/concrete/GeminiProModel.py
@@ -1,6 +1,7 @@
 import json
 from typing import List
 import google.generativeai as genai
+from swarmauri.core.messages.IMessage import IMessage
 from swarmauri.standard.llms.base.LLMBase import LLMBase
 
 
@@ -9,6 +10,27 @@ class GeminiProModel(LLMBase):
     allowed_models: List[str] = ['gemini-1.5-pro-latest']
     name: str = "gemini-1.5-pro-latest"
 
+    def _format_messages(self, messages: List[IMessage]) -> List[Dict[str, str]]:
+        # Remove system instruction from messages
+        message_properties = ['content', 'role']
+        sanitized_messages = [message.dict(include=message_properties) for message in messages 
+            if message['role'] != 'system']
+
+        for message in sanitized_messages:
+            if message['role'] == 'assistant':
+                message['role'] = 'model'
+
+            # update content naming
+            message['parts'] = message['content']
+
+        return sanitized_messages
+
+    def _get_system_context(self, messages:List[IMessage]) -> str:
+        system_context = None
+        for message in messages:
+            if message.role == 'system':
+                system_context = message.content
+        return system_context
     
     def predict(self, messages, temperature=0.7, max_tokens=256):
         genai.configure(api_key=self.api_key)
@@ -39,40 +61,19 @@ class GeminiProModel(LLMBase):
         ]
 
 
-        system_context = None
-        for message in messages:
-            # update role naming
-            if message.role == 'assistant':
-                message.role = 'model'
-
-            if message.role == 'system':
-                system_context = message.content
-
-            # update content naming
-            message['parts'] = message.content
-
-        
-        # Remove system instruction from messages
-        sanitized_messages = [message for message in messages if message['role'] != 'system'] 
-
-        # if we remove more than one system message from the array
-        # then we know we were given too many system messages
-        # we raise an error in this scenario.
-        # Examples:
-        # 10 - 1 = 9 so if 9 + 1 < 10  then negative is good
-        # 10 - 2 = 8, so if 8 + 1 < 10 then positive is bad
-        if len(sanitized_messages) + 1 > len(messages):
-            raise ValueError('cannot send an array of conversations containing more than one system instruction.')
+        system_context = self._get_system_context(messages)
+        formatted_messages = self._format_messages(messages)
 
 
-        next_message = sanitized_messages.pop()
+        next_message = formatted_messages.pop()
 
         client = genai.GenerativeModel(model_name=self.name,
             safety_settings=safety_settings,
-            generation_config=generation_config)
+            generation_config=generation_config,
+            system_instruction=system_context)
 
         convo = client.start_chat(
-            history=sanitized_messages,
+            history=formatted_messages,
             )
 
         convo.send_message(next_message['parts'])
