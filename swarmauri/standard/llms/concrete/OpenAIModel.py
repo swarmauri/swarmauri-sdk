0781d8a5916f5852ad0649aebb1898e342a8a8de
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-18 18:50:33 -0500

    📛 v0.3.3.dev112 - F821

--- swarmauri/standard/llms/concrete/OpenAIModel.py
+++ swarmauri/standard/llms/concrete/OpenAIModel.py
@@ -1,5 +1,5 @@
 import json
-from typing import List
+from typing import List, Dict
 from openai import OpenAI
 from swarmauri.core.messages.IMessage import IMessage
 from swarmauri.standard.llms.base.LLMBase import LLMBase
