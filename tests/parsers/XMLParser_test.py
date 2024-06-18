aea554093b77cb6ac40e1d3d5ab3b7c58f9093f8
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-18 14:47:34 -0500

    🐛v0.3.3.dev89 - fix Assertion Value

--- tests/parsers/XMLParser_test.py
+++ tests/parsers/XMLParser_test.py
@@ -12,7 +12,7 @@ def test_ubc_resource():
 def test_parse():
     def test():
         documents = XMLParser(element_tag='project').parse('<root><project>stuff inside project</project><project>test</project></root>')
-        assert len(documents) == 1
+        assert len(documents) == 2
         assert documents[0].resource == 'Document'
         assert documents[0].content == 'stuff inside project'
         assert documents[1].resource == 'Document'
