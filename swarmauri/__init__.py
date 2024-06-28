711996fac6d1ee6d0ae0873c986da3e399892b72
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-06-28 14:23:50 -0500

    v0.3.5.dev20 - reorder imports to debug subclassunion

--- swarmauri/__init__.py
+++ swarmauri/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.5.dev19"
+__version__ = "0.3.5.dev20"
 __long_desc__ = """
 # swarmaURI sdk
 
--- tests/agents/RagAgent_test.py
+++ tests/agents/RagAgent_test.py
@@ -1,11 +1,11 @@
 import pytest
 import os
 from swarmauri.standard.llms.concrete.GroqModel import GroqModel
-from swarmauri.standard.agents.concrete.RagAgent import RagAgent
 from swarmauri.standard.conversations.concrete.Conversation import Conversation
 from swarmauri.standard.vector_stores.concrete.TfidfVectorStore import TfidfVectorStore
 from swarmauri.standard.messages.concrete.SystemMessage import SystemMessage
 from swarmauri.standard.documents.concrete.Document import Document
+from swarmauri.standard.agents.concrete.RagAgent import RagAgent
 
 @pytest.mark.unit
 def test_ubc_resource():
