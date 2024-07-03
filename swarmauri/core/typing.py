a2824f0de4755e85ac38402a9b45dd0012d88bee
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-07-03 07:05:26 -0500

    🩸v0.3.7.dev6

--- swarmauri/core/typing.py
+++ swarmauri/core/typing.py
@@ -1,32 +1,17 @@
 import logging
 from pydantic import BaseModel, Field
 from typing import TypeVar, Generic, Union, Annotated, Type
-from swarmauri.core.ComponentBase import ComponentBase
 
-ComponentType = TypeVar('ComponentType', bound=ComponentBase)
 
 class SubclassUnion(Generic[ComponentType]):
-    @staticmethod
-    def __get_validators__():
-        yield SubclassUnion.validate
 
     @classmethod
-    def validate(cls, v, field):
-        obj = field.sub_fields[0].type_ if field.sub_fields else None
-        if obj and issubclass(obj, ComponentBase):
-            subclasses = tuple(obj.__swm__get_subclasses__())
-            if isinstance(v, subclasses):
-                return v
-            raise TypeError(f'Value must be an instance of {subclasses}')
-        raise TypeError('Invalid object type')
-
-    @classmethod
-    def __class_getitem__(cls, baseclass: Type[ComponentType]):
+    def __class_getitem__(cls, baseclass:):
         subclasses = cls.__swm__get_subclasses__(baseclass)
         return Union[tuple(subclasses)]
 
     @classmethod
-    def __swm__get_subclasses__(cls, baseclass: Type[ComponentType]) -> set:
+    def __swm__get_subclasses__(cls, baseclass) -> set:
         logging.debug('__swm__get_subclasses__ executed\n')
         def is_excluded_module(module_name: str) -> bool:
             return (module_name == 'builtins' or 
