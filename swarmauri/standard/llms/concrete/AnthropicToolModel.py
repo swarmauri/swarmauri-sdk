c7068602a6d478d55a9a09c2fba5e40fc2a03900
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-07-03 10:44:58 -0500

    v0.3.8.dev6 - AnthropicToolModel

--- swarmauri/standard/llms/concrete/AnthropicToolModel.py
+++ swarmauri/standard/llms/concrete/AnthropicToolModel.py
@@ -1,5 +1,6 @@
 import json
 from typing import List, Dict, Literal, Any
+import logging
 import anthropic
 from swarmauri.core.messages.IMessage import IMessage
 from swarmauri.standard.llms.base.LLMBase import LLMBase
@@ -17,10 +18,12 @@ class AnthropicToolModel(LLMBase):
     type: Literal['AnthropicToolModel'] = 'AnthropicToolModel'
     
     def _schema_convert_tools(self, tools) -> List[Dict[str, Any]]:
-        return [AnthropicSchemaConverter().convert(tools[tool]) for tool in tools]
+        schema_result = [AnthropicSchemaConverter().convert(tools[tool]) for tool in tools]
+        logging.info(schema_result)
+        return schema_result
 
     def _format_messages(self, messages: List[IMessage]) -> List[Dict[str, str]]:
-        message_properties = ['content', 'role', 'name', 'tool_call_id', 'tool_calls']
+        message_properties = ['content', 'role', 'tool_call_id', 'tool_calls']
         formatted_messages = [message.model_dump(include=message_properties, exclude_none=True) for message in messages]
         return formatted_messages
 
