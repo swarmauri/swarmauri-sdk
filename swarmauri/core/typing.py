02516035a76c40493179c53b0e1274b2ed4d3ab8
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-07-03 06:55:15 -0500

    v0.3.7.dev5

--- swarmauri/core/typing.py
+++ swarmauri/core/typing.py
@@ -1,7 +1,9 @@
 import logging
 from pydantic import BaseModel, Field
 from typing import TypeVar, Generic, Union, Annotated, Type
-from swarmauri.core.ComponentBase import ComponentBase, ComponentType
+from swarmauri.core.ComponentBase import ComponentBase
+
+ComponentType = TypeVar('ComponentType', bound=ComponentBase)
 
 class SubclassUnion(Generic[ComponentType]):
     @staticmethod
@@ -20,5 +22,20 @@ class SubclassUnion(Generic[ComponentType]):
 
     @classmethod
     def __class_getitem__(cls, baseclass: Type[ComponentType]):
-        subclasses = baseclass.__swm__get_subclasses__()
+        subclasses = cls.__swm__get_subclasses__(baseclass)
         return Union[tuple(subclasses)]
+
+    @classmethod
+    def __swm__get_subclasses__(cls, baseclass: Type[ComponentType]) -> set:
+        logging.debug('__swm__get_subclasses__ executed\n')
+        def is_excluded_module(module_name: str) -> bool:
+            return (module_name == 'builtins' or 
+                    module_name == 'types')
+
+        subclasses_dict = {baseclass.__name__: baseclass}
+        for subclass in baseclass.__subclasses__():
+            if not is_excluded_module(subclass.__module__):
+                subclasses_dict.update({_s.__name__: _s for _s in cls.__swm__get_subclasses__(subclass) 
+                    if not is_excluded_module(_s.__module__)})
+
+        return set(subclasses_dict.values())
