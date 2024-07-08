3bb9857ac6cd5b8b49c4b0c4686134e62ffecc46
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-07-07 22:39:57 -0500

    🐛v0.3.8.dev96 - rm default name on humanmessage to check for failures

--- swarmauri/__init__.py
+++ swarmauri/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.8.dev95"
+__version__ = "0.3.8.dev96"
 __long_desc__ = """
 # swarmaURI sdk
 
--- swarmauri/standard/messages/concrete/HumanMessage.py
+++ swarmauri/standard/messages/concrete/HumanMessage.py
@@ -5,5 +5,5 @@ from swarmauri.standard.messages.base.MessageBase import MessageBase
 class HumanMessage(MessageBase):
     content: str
     role: str = Field(default='user')
-    name: str = Field(default='user')
+    name: str
     type: Literal['HumanMessage'] = 'HumanMessage'    
