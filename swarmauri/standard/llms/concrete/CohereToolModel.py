a403230c45e42a742dc0c85e89be6df541cc667c
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-07-08 07:22:09 -0500

    🐛v0.3.8.dev111 - rm if/else stmt

--- swarmauri/standard/llms/concrete/CohereToolModel.py
+++ swarmauri/standard/llms/concrete/CohereToolModel.py
@@ -41,19 +41,14 @@ class CohereToolModel(LLMBase):
         logging.info(f"_schema_convert_tools: {self._schema_convert_tools(toolkit.tools)}")
         logging.info(f"message: {formatted_messages[-1]}")
         logging.info(f"formatted_messages: {formatted_messages}")
-        if len(formatted_messages) > 1:
-            tool_response = client.chat(
-                model=self.name, 
-                message=formatted_messages[-1]['content'], 
-                chat_history=formatted_messages[:-1],
-                tools=self._schema_convert_tools(toolkit.tools)
-            )
-        else:
-            tool_response = client.chat(
-                model=self.name, 
-                message=formatted_messages[-1]['content'],
-                tools=self._schema_convert_tools(toolkit.tools)
-            )
+
+        tool_response = client.chat(
+            model=self.name, 
+            message=formatted_messages[-1]['content'], 
+            chat_history=formatted_messages[:-1],
+            tools=self._schema_convert_tools(toolkit.tools)
+        )
+
 
         formatted_messages.append(tool_response)
 
