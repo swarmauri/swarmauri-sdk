ad7b5dc2b4ad96bad3945e372d0ffc24cb59cd8b
faizan2700 <syedfaizan824@gmail.com>
2024-08-15 18:46:41 +0530

    updated shuttleaitool model tests

--- swarmauri/standard/llms/concrete/ShuttleAIToolModel.py
+++ swarmauri/standard/llms/concrete/ShuttleAIToolModel.py
@@ -81,14 +81,21 @@ class ShuttleAIToolModel(LLMBase):
             payload['citations'] = citations  
 
         agent_response = requests.request("POST", url, json=payload, headers=headers) 
-        messages = [formatted_messages[-1], agent_response.json()['choices'][0]['message']['content']]
+        try: 
+            messages = [formatted_messages[-1], agent_response.json()['choices'][0]['message']['content']]
+        except Exception as error: 
+            print(str(error)) 
+            print(agent_response.json()) 
         tool_calls = agent_response.json()['choices'][0]['message'].get('tool_calls', None) 
         if tool_calls:
             for tool_call in tool_calls:
                 func_name = tool_call['function']['name'] 
                 func_call = toolkit.get_tool_by_name(func_name)
-                func_args = json.loads(tool_call['function']['arguments'])
-                func_result = func_call(**func_args)
+                func_args = json.loads(tool_call['function']['arguments']) 
+                try: 
+                    func_result = func_call(**func_args)
+                except Exception as error: 
+                    func_result = 'error' 
                 messages.append(
                     {
                         "tool_call_id": tool_call.id,
