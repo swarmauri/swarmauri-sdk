d2c9198be717a00eb10eecc443deeb8b6360eb57
Abdulsamod Azeez <abdulsamod2@gmail.com>
2024-08-20 14:44:22 +0100

    Update GeminiSchemaConverter_test.py

--- tests/schema_converters/GeminiSchemaConverter_test.py
+++ tests/schema_converters/GeminiSchemaConverter_test.py
@@ -4,6 +4,7 @@ import json
 from swarmauri.standard.tools.concrete.AdditionTool import AdditionTool
 from swarmauri.standard.toolkits.concrete.Toolkit import Toolkit
 from swarmauri.standard.agents.concrete.ToolAgent import ToolAgent
+from google.protobuf.json_format import MessageToJson
 
 from swarmauri.standard.schema_converters.concrete.GeminiSchemaConverter import (
     GeminiSchemaConverter as Schema,
@@ -32,6 +33,7 @@ def test_convert():
     toolkit = Toolkit()
     tool = AdditionTool()
     toolkit.add_tool(tool)
-    result = [Schema().convert(toolkit.tools[tool]) for tool in toolkit.tools]
+    schema = Schema()
+    result = [MessageToJson(schema.convert(toolkit.tools[tool])) for tool in toolkit.tools]
     logging.info(result)
     assert json.loads(json.dumps(result))
