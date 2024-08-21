4b7e44e859150780aa2ba5a7be375796e2fd9dc3
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-08-21 14:21:34 -0500

    🐛🔨 FIX E999

--- swarmauri/standard/llms/concrete/ShuttleAIModel.py
+++ swarmauri/standard/llms/concrete/ShuttleAIModel.py
@@ -33,7 +33,7 @@ class ShuttleAIModel(LLMBase):
         message_properties = ["content", "role"]
 
         # Exclude FunctionMessages
-        formatted_messages = [message.model_dump(include=message_properties) for message]
+        formatted_messages = [message.model_dump(include=message_properties) for message in messages]
         return formatted_messages
 
     
