bea309e644eeded97bbba632b7073b54157ad81a
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-08-28 11:08:56 -0500

    🐛adding missing test cases

---
+++ tests/tools/FleschKincaid_test.py
@@ -0,0 +1,21 @@
+import pytest
+from swarmauri.standard.tools.concrete.FleschKincaid import FleschKincaid as Tool
+
+@pytest.mark.unit
+def test_ubc_resource():
+    tool = Tool()
+    assert tool.resource == 'Tool'
+
+@pytest.mark.unit
+def test_ubc_type():
+    assert Tool().type == 'FleschKincaid'
+
+@pytest.mark.unit
+def test_initialization():
+    tool = Tool()
+    assert type(tool.id) == str
+
+@pytest.mark.unit
+def test_call():
+    tool = Tool()
+    assert tool(10, 10) == str(20)
---
+++ tests/tools/SMOGIndex_test.py
@@ -0,0 +1,21 @@
+import pytest
+from swarmauri.standard.tools.concrete.SMOGIndex import SMOGIndex as Tool
+
+@pytest.mark.unit
+def test_ubc_resource():
+    tool = Tool()
+    assert tool.resource == 'Tool'
+
+@pytest.mark.unit
+def test_ubc_type():
+    assert Tool().type == 'SMOGIndex'
+
+@pytest.mark.unit
+def test_initialization():
+    tool = Tool()
+    assert type(tool.id) == str
+
+@pytest.mark.unit
+def test_call():
+    tool = Tool()
+    assert tool(10, 10) == str(20)
