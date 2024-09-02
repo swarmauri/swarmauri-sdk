26f577211bcb9d13a6654d2417d7244c0da6cdfc
JustusOmbok <ombokjustus@gmail.com>
2024-09-02 17:40:22 +0300

    Added files to accessibity toolkit

--- swarmauri/standard/toolkits/concrete/AccessibilityToolkit.py
+++ swarmauri/standard/toolkits/concrete/AccessibilityToolkit.py
@@ -1,21 +1,27 @@
 from typing import Literal
 from swarmauri.standard.tools.concrete.AutomatedReadabilityIndexTool import AutomatedReadabilityIndexTool
 from swarmauri.standard.tools.concrete.ColemanLiauIndexTool import ColemanLiauIndexTool
-from swarmauri.standard.tools.concrete.DaleChallReadabilityTool import DaleChallReadabilityTool
 from swarmauri.standard.tools.concrete.FleschKincaidTool import FleschKincaidTool
 from swarmauri.standard.tools.concrete.FleschReadingEaseTool import FleschReadingEaseTool
 from swarmauri.standard.tools.concrete.GunningFogTool import GunningFogTool
-from swarmauri.standard.tools.concrete.LexicalDensityTool import LexicalDensityTool
-from swarmauri.standard.tools.concrete.TextLengthTool import TextLengthTool
 from swarmauri.standard.toolkits.base.ToolkitBase import ToolkitBase
 
 class AccessibilityToolkit(ToolkitBase):
     type: Literal['AccessibilityToolkit'] = 'AccessibilityToolkit'
+    resource: Literal['AccessibilityToolkit'] = 'AccessibilityToolkit'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.tools = []
         self.add_tool(AutomatedReadabilityIndexTool(name='AutomatedReadabilityIndexTool'))
         self.add_tool(ColemanLiauIndexTool(name='ColemanLiauIndexTool'))
         self.add_tool(FleschKincaidTool(name='FleschKincaidTool'))
         self.add_tool(FleschReadingEaseTool(name='FleschReadingEaseTool'))
         self.add_tool(GunningFogTool(name='GunningFogTool'))
+
+    def add_tool(self, tool):
+        self.tools.append(tool)
+
+    def get_tool_names(self):
+        # Returns a list of tool names in the toolkit
+        return [tool.name for tool in self.tools]
--- tests/toolkits/AccessibilityToolkit_test.py
+++ tests/toolkits/AccessibilityToolkit_test.py
@@ -1,5 +1,10 @@
 import pytest
-from swarmauri.standard.tools.concrete.TextLengthTool import TextLengthTool as Tool
+from swarmauri.standard.tools.concrete.AutomatedReadabilityIndexTool import AutomatedReadabilityIndexTool
+from swarmauri.standard.tools.concrete.ColemanLiauIndexTool import ColemanLiauIndexTool
+from swarmauri.standard.tools.concrete.FleschKincaidTool import FleschKincaidTool
+from swarmauri.standard.tools.concrete.FleschReadingEaseTool import FleschReadingEaseTool
+from swarmauri.standard.tools.concrete.GunningFogTool import GunningFogTool
+from swarmauri.standard.toolkits.base.ToolkitBase import ToolkitBase
 from swarmauri.standard.toolkits.concrete.AccessibilityToolkit import AccessibilityToolkit as Toolkit
 
 @pytest.mark.unit
@@ -14,23 +19,27 @@ def test_ubc_type():
 
 @pytest.mark.unit
 def test_serialization():
-    toolkit = Toolkit()
-    tool_name = 'TextLengthTool'
-    tool = Tool(name=tool_name)
-    toolkit.add_tool(tool)
-    assert toolkit.id == Toolkit.model_validate_json(toolkit.model_dump_json()).id
-    assert toolkit.get_tool_by_name(tool_name)('hello there!') == {'num_characters': 11, 'num_words': 3, 'num_sentences': 1}
+    # Create an instance of AccessibilityToolkit
+    toolkit = AccessibilityToolkit()
+
+    # Serialize the toolkit to JSON
+    serialized_data = toolkit.model_dump_json()
+
+    # Deserialize the JSON back to an AccessibilityToolkit object
+    deserialized_toolkit = AccessibilityToolkit.model_validate_json(serialized_data)
+
+    # Assert that the ID and content of the toolkit remain the same after serialization and deserialization
+    assert toolkit.id == deserialized_toolkit.id
+    assert toolkit.get_tool_names() == deserialized_toolkit.get_tool_names()
+
+    # Optionally, ensure that the entire object is equal, including the state of each tool
+    assert toolkit == deserialized_toolkit
 
 @pytest.mark.unit
 def test_add_tool():
     toolkit = Toolkit()
-    tool = Tool()
-    toolkit.add_tool(tool)
-    assert len(toolkit.get_tools()) == 1
-
-@pytest.mark.unit
-def test_call_textlength():
-    toolkit = Toolkit()
-    tool_name = 'TextLengthTool'
-    expected_result = {'num_characters': 11, 'num_words': 3, 'num_sentences': 1}
-    assert toolkit.get_tool_by_name(tool_name)('hello there!') == expected_result
+    initial_tool_count = len(toolkit.get_tool_names())
+    # Replace Tool() with an actual tool class or instance if available
+    new_tool = AutomatedReadabilityIndexTool(name='NewTool')
+    toolkit.add_tool(new_tool)
+    assert len(toolkit.get_tool_names()) == initial_tool_count + 1
