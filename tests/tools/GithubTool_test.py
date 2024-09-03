900f0a828a34b943944661fc947fe60ab2e065a8
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-09-03 08:54:04 +0100

    fixed a test call error in PsutilTool_test.py

---
+++ tests/tools/GithubTool_test.py
@@ -0,0 +1,26 @@
+import pytest
+from swarmauri.community.tools.concrete.GithubTool import GithubTool as Tool
+
+@pytest.mark.unit
+def test_ubc_resource():
+    tool = Tool()
+    assert tool.resource == 'Tool'
+
+@pytest.mark.unit
+def test_ubc_type():
+    assert Tool().type == 'GithubTool'
+
+@pytest.mark.unit
+def test_initialization():
+    tool = Tool()
+    assert type(tool.id) == str
+
+@pytest.mark.unit
+def test_serialization():
+    tool = Tool()
+    assert tool.id == Tool.model_validate_json(tool.model_dump_json()).id
+
+@pytest.mark.unit
+def test_call():
+    tool = Tool()
+    assert tool("") == "dummy test message"
