35db68af4caf69a544885bad547b9696c80e9fbc
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-09-09 22:18:30 +0100

    fixed error in test_call functions relating to CodeExtractorTool_test.py

--- tests/tools/CodeExtractorTool_test.py
+++ tests/tools/CodeExtractorTool_test.py
@@ -34,30 +34,6 @@ def test_serialization():
     tool = Tool()
     assert tool.id == Tool.model_validate_json(tool.model_dump_json()).id
 
-
-@pytest.mark.unit
-def test_call():
-    """
-    Test the __call__ method of CodeExtractorTool.
-    """
-    file_content = """\"\"\" module documentation string\n \"\"\"
-function1 = lambda x: x + 1
-variable1 = 10
-print('Hello, World!')"""
-
-    with tempfile.NamedTemporaryFile("w+", delete=False) as file:
-        file.write(file_content)
-        file_name = file.name
-
-    tool = Tool()
-    extract_documentation = True
-    to_be_ignored = ["function1", "variable1"]
-    expected_code = """\"\"\" module documentation string\n \"\"\"
-print('Hello, World!')"""
-
-    extracted_code = tool(file_name, extract_documentation, to_be_ignored)
-    assert extracted_code == expected_code
-
 @pytest.mark.unit
 def test_extract_code():
     file_content = """\"\"\" module documentation string\n \"\"\"
@@ -183,7 +159,7 @@ variable1 = 10"""
         ),
     ]
 )
-def test_tool(file_content, extract_documentation, to_be_ignored, expected_code):
+def test_call(file_content, extract_documentation, to_be_ignored, expected_code):
     with tempfile.NamedTemporaryFile("w+", delete=False) as file:
         file.write(file_content)
         file_name = file.name
