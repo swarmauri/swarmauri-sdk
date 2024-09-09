ed3828987330d90ade53bfed22a88d3ea505c01e
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-09-09 10:46:38 -0500

    📛FIX F821

--- swarmauri/community/tools/concrete/EntityRecognitionTool.py
+++ swarmauri/community/tools/concrete/EntityRecognitionTool.py
@@ -3,7 +3,7 @@ from typing import List, Literal, Dict
 from transformers import pipeline, logging as hf_logging
 from swarmauri.standard.tools.base.ToolBase import ToolBase
 from swarmauri.standard.tools.concrete.Parameter import Parameter
-
+from pydantic import Field
 
 hf_logging.set_verbosity_error()
 
