9ada1103a2b18549e62568f9920bd2999cf56c6e
3rdSon <nnajivictorious@gmail.com>
2024-09-11 15:54:57 +0100

    fixed ChainStep_test and ContextChain_test

--- tests/chains/ChainStep_test.py
+++ tests/chains/ChainStep_test.py
@@ -2,32 +2,44 @@ import pytest
 from swarmauri.standard.chains.concrete.ChainStep import ChainStep
 from swarmauri.standard.tools.concrete.AdditionTool import AdditionTool
 
+
 @pytest.mark.unit
 def test_ubc_resource():
     tool = AdditionTool()
-    step = ChainStep(key='test', method=tool, args=(1,2), kwargs={}, ref=None)
-    assert step.resource == 'ChainStep'
+    step = ChainStep(key="test", method=tool, args=(1, 2), kwargs={}, ref=None)
+    assert step.resource == "ChainStep"
 
 
 @pytest.mark.unit
 def test_ubc_type():
     tool = AdditionTool()
-    step = ChainStep(key='test', method=tool, args=(1,2), kwargs={}, ref=None)
-    assert step.type == 'ChainStep'
+    step = ChainStep(key="test", method=tool, args=(1, 2), kwargs={}, ref=None)
+    assert step.type == "ChainStep"
+
 
 @pytest.mark.unit
 def test_method_args_only_call():
     tool = AdditionTool()
-    args = (1,2)
+    args = (1, 2)
     kwargs = {}
-    step = ChainStep(key='test', method=tool, args=args, kwargs=kwargs, ref=None)
+    step = ChainStep(key="test", method=tool, args=args, kwargs=kwargs, ref=None)
     result = step.method(*step.args, **step.kwargs)
-    assert result[0] == '3'
+
+    assert isinstance(result, dict)
+    assert "sum" in result
+    assert result["sum"] == "3"
+
 
 @pytest.mark.unit
 def test_serialization():
     tool = AdditionTool()
-    args = (1,2)
+    args = (1, 2)
     kwargs = {}
-    step = ChainStep(key='test', method=tool, args=args, kwargs=kwargs, ref=None)
-    assert step.id == ChainStep.model_validate_json(step.model_dump_json()).id
+    step = ChainStep(key="test", method=tool, args=args, kwargs=kwargs, ref=None)
+
+    # Serialize and deserialize
+    serialized_step = step.model_dump_json()
+    deserialized_step = ChainStep.model_validate_json(serialized_step)
+
+    # Assert that the method (tool) remains the same after serialization and deserialization
+    assert isinstance(deserialized_step.method, AdditionTool)
