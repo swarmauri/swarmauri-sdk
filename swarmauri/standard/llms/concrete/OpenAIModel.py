2e9ba4f20ca7c05000c8306167a6eeb94d1d6869
3rdSon <nnajivictorious@gmail.com>
2024-09-18 10:12:56 +0100

    updated allowed models in openai

--- swarmauri/standard/llms/concrete/OpenAIModel.py
+++ swarmauri/standard/llms/concrete/OpenAIModel.py
@@ -27,6 +27,16 @@ class OpenAIModel(LLMBase):
         "gpt-3.5-turbo-instruct",
         "o1-preview",
         "o1-mini",
+        "gpt-4o-2024-05-13",
+        "gpt-4o-2024-08-06",
+        "gpt-4o-mini-2024-07-18",
+        "o1-preview-2024-09-12",
+        "o1-mini-2024-09-12",
+        "gpt-4-turbo-2024-04-09",
+        "gpt-4-0125-preview",
+        "gpt-4-0613",
+        "gpt-4-0314",
+        "gpt-3.5-turbo-0125",
     ]
     name: str = "gpt-3.5-turbo"
     type: Literal["OpenAIModel"] = "OpenAIModel"
