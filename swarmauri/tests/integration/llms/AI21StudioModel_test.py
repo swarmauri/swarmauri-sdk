1eb0e7290f47e68d4bf700f9faa5c748ea0cb5eb
RafaelJohn9 <rafaeljohb@gmail.com>
2024-09-20 18:16:09 +0300

    Refactor: Test files in groups of units, integration and expected_to_fail

---
+++ swarmauri/tests/integration/llms/AI21StudioModel_test.py
@@ -0,0 +1,75 @@
+import os
+import pytest
+from swarmauri.standard.llms.concrete.AI21StudioModel import AI21StudioModel as LLM
+from swarmauri.standard.conversations.concrete.Conversation import Conversation
+
+from swarmauri.standard.messages.concrete.AgentMessage import AgentMessage
+from swarmauri.standard.messages.concrete.HumanMessage import HumanMessage
+from swarmauri.standard.messages.concrete.SystemMessage import SystemMessage
+
+
+@pytest.mark.skipif(
+    not os.getenv("AI21STUDIO_API_KEY"),
+    reason="Skipping due to environment variable not set",
+)
+@pytest.mark.acceptance
+def test_nonpreamble_system_context():
+    API_KEY = os.getenv("AI21STUDIO_API_KEY")
+    model = LLM(api_key=API_KEY)
+    conversation = Conversation()
+
+    # Say hi
+    input_data = "Hi"
+    human_message = HumanMessage(content=input_data)
+    conversation.add_message(human_message)
+
+    # Get Prediction
+    model.predict(conversation=conversation)
+
+    # Give System Context
+    system_context = 'You only respond with the following phrase, "Jeff"'
+    human_message = SystemMessage(content=system_context)
+    conversation.add_message(human_message)
+
+    # Prompt
+    input_data = "Hi"
+    human_message = HumanMessage(content=input_data)
+    conversation.add_message(human_message)
+
+    model.predict(conversation=conversation)
+    prediction = conversation.get_last().content
+    assert "Jeff" in prediction
+
+
+@pytest.mark.skipif(
+    not os.getenv("AI21STUDIO_API_KEY"),
+    reason="Skipping due to environment variable not set",
+)
+@pytest.mark.acceptance
+def test_multiple_system_contexts():
+    API_KEY = os.getenv("AI21STUDIO_API_KEY")
+    model = LLM(api_key=API_KEY)
+    conversation = Conversation()
+
+    system_context = 'You only respond with the following phrase, "Jeff"'
+    human_message = SystemMessage(content=system_context)
+    conversation.add_message(human_message)
+
+    input_data = "Hi"
+    human_message = HumanMessage(content=input_data)
+    conversation.add_message(human_message)
+
+    prediction = model.predict(conversation=conversation)
+
+    system_context_2 = 'You only respond with the following phrase, "Ben"'
+    human_message = SystemMessage(content=system_context_2)
+    conversation.add_message(human_message)
+
+    input_data_2 = "Hey"
+    human_message = HumanMessage(content=input_data_2)
+    conversation.add_message(human_message)
+
+    model.predict(conversation=conversation)
+    prediction = conversation.get_last().content
+    assert type(prediction) == str
+    assert "Ben" in prediction
---
+++ swarmauri/tests/unit/prompts/PromptMatrix_test.py
@@ -0,0 +1,33 @@
+import pytest
+from swarmauri.standard.prompts.concrete.PromptMatrix import PromptMatrix
+
+@pytest.mark.unit
+def test_ubc_resource():
+	prompt_matrix = PromptMatrix(matrix=[["1","2"],["3","4"]])
+	assert prompt_matrix.resource == 'Prompt'
+
+@pytest.mark.unit
+def test_ubc_type():
+	prompt_matrix = PromptMatrix(matrix=[["1","2"],["3","4"]])
+	assert prompt_matrix.type == 'PromptMatrix'
+
+@pytest.mark.unit
+def test_serialization():
+	prompt_matrix = PromptMatrix(matrix=[["1","2"],["3","4"]])
+	assert prompt_matrix.id == PromptMatrix.model_validate_json(prompt_matrix.model_dump_json()).id
+
+@pytest.mark.unit
+def shape_interface_test():
+	prompt_matrix = PromptMatrix(matrix=[["1","2"],["3","4"]])
+	assert prompt_matrix.shape == (2,2)
+
+@pytest.mark.unit
+def show_matrix_test():
+	prompt_matrix = PromptMatrix(matrix=[["1","2"],["3","4"]])
+	assert prompt_matrix.show() == [['1','2'], ['3','4']]
+
+
+
+
+
+
