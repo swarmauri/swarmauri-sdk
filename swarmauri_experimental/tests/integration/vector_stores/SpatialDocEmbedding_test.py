560bf83bd063cd803352590599f15c5da4be7e40
3rdSon <nnajivictorious@gmail.com>
2024-09-20 14:25:54 +0100

    corrected import in experimental and splitted the testfiles

--- swarmauri_experimental/tests/integration/vector_stores/SpatialDocEmbedding_test.py
+++ swarmauri_experimental/tests/integration/vector_stores/SpatialDocEmbedding_test.py
@@ -12,20 +12,6 @@ def test_ubc_resource():
     test()
 
 
-@pytest.mark.unit
-def test_ubc_type():
-    assert SpatialDocEmbedding().type == "SpatialDocEmbedding"
-
-
-@pytest.mark.unit
-def test_serialization():
-    embedder = SpatialDocEmbedding()
-    assert (
-        embedder.id
-        == SpatialDocEmbedding.model_validate_json(embedder.model_dump_json()).id
-    )
-
-
 @pytest.mark.acceptance
 def test_fit_transform():
     embedder = SpatialDocEmbedding()
