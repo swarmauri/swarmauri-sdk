6e89283c3de1877ef138498179c08f7afa0209bd
3rdSon <nnajivictorious@gmail.com>
2024-09-20 12:06:19 +0100

    corrected import statemtents in experimental

--- swarmauri_experimental/experimental/models/SageMaker.py
+++ swarmauri_experimental/experimental/models/SageMaker.py
@@ -1,10 +1,12 @@
 import json
 import boto3
-from ...core.models.IModel import IModel
+from swarmauri.core.models.IModel import IModel
 
 
 class AWSSageMakerModel(IModel):
-    def __init__(self, access_key: str, secret_access_key: str, region_name: str, model_name: str):
+    def __init__(
+        self, access_key: str, secret_access_key: str, region_name: str, model_name: str
+    ):
         """
         Initialize the AWS SageMaker model with AWS credentials, region, and the model name.
 
@@ -17,26 +19,30 @@ class AWSSageMakerModel(IModel):
         self.access_key = access_key
         self.secret_access_key = secret_access_key
         self.region_name = region_name
-        self.client = boto3.client('sagemaker-runtime',
-                                   aws_access_key_id=access_key,
-                                   aws_secret_access_key=secret_access_key,
-                                   region_name=region_name)
+        self.client = boto3.client(
+            "sagemaker-runtime",
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_access_key,
+            region_name=region_name,
+        )
         super().__init__(model_name)
 
-    def predict(self, payload: str, content_type: str='application/json') -> dict:
+    def predict(self, payload: str, content_type: str = "application/json") -> dict:
         """
         Generate predictions using the AWS SageMaker model.
 
         Parameters:
         - payload (str): Input data in JSON format.
         - content_type (str): The MIME type of the input data (default: 'application/json').
-        
+
         Returns:
         - dict: The predictions returned by the model.
         """
-        endpoint_name = self.model_name  # Assuming the model name is also the endpoint name
-        response = self.client.invoke_endpoint(EndpointName=endpoint_name,
-                                               Body=payload,
-                                               ContentType=content_type)
-        result = json.loads(response['Body'].read().decode())
+        endpoint_name = (
+            self.model_name
+        )  # Assuming the model name is also the endpoint name
+        response = self.client.invoke_endpoint(
+            EndpointName=endpoint_name, Body=payload, ContentType=content_type
+        )
+        result = json.loads(response["Body"].read().decode())
         return result
