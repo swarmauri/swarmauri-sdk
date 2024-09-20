e0c802296073e188d7879a448357c9d34b1f0bda
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-09-20 13:00:28 +0100

    swarmauri package refactoring

---
+++ swarmauri_community/tests/toolkits/unit/GithubToolkit_test.py
@@ -0,0 +1,99 @@
+import os
+from unittest.mock import patch, MagicMock
+import pytest
+import logging
+from swarmauri.standard.tools.concrete.AdditionTool import AdditionTool
+from swarmauri_community.community.tools.concrete.GithubRepoTool import GithubRepoTool
+from swarmauri_community.community.toolkits.concrete.GithubToolkit import (
+    GithubToolkit as Toolkit,
+)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("GITHUBTOOL_TEST_TOKEN")
+
+
+@pytest.mark.unit
+def test_ubc_resource():
+    toolkit = Toolkit(token=token)
+    assert toolkit.resource == "GithubToolkit"
+
+
+@pytest.mark.unit
+def test_ubc_type():
+    toolkit = Toolkit(token=token)
+    assert toolkit.type == "GithubToolkit"
+
+
+@pytest.mark.unit
+def test_serialization():
+    toolkit = Toolkit(token=token)
+
+    serialized_data = toolkit.model_dump_json()
+    logging.info(serialized_data)
+
+    deserialized_toolkit = Toolkit.model_validate_json(serialized_data)
+
+    assert toolkit.id == deserialized_toolkit.id
+
+
+@pytest.mark.unit
+def test_tool_count():
+    toolkit = Toolkit(token=token)
+    assert len(toolkit.get_tools()) == 5
+
+
+@pytest.mark.unit
+def test_add_tool():
+    toolkit = Toolkit(token=token)
+    tool = AdditionTool()
+    toolkit.add_tool(tool)
+    assert len(toolkit.get_tools()) == 6
+
+
+@pytest.mark.parametrize(
+    "action, kwargs, method_called",
+    [
+        # Valid cases for repo management
+        ("create_repo", {"repo_name": "test-repo"}, "create_repo"),
+        ("delete_repo", {"repo_name": "test-repo"}, "delete_repo"),
+        ("get_repo", {"repo_name": "test-repo"}, "get_repo"),
+        ("list_repos", {}, "list_repos"),
+        ("update_repo", {"repo_name": "test-repo"}, "update_repo"),
+        # Invalid action
+        ("invalid_action", {}, None),
+    ],
+)
+@pytest.mark.unit
+@patch("swarmauri_community.community.tools.concrete.GithubRepoTool.Github")
+def test_call_github_repo_tool(mock_github, action, kwargs, method_called):
+    expected_keys = {action}
+    toolkit = Toolkit(token=token)
+    tool_name = "GithubRepoTool"
+
+    mock_github.return_value = MagicMock()
+
+    if method_called is not None:
+        with patch.object(
+            GithubRepoTool,
+            method_called,
+            return_value="performed a test action successfully",
+        ) as mock_method:
+            result = toolkit.get_tool_by_name(tool_name)(action=action, **kwargs)
+
+            mock_method.assert_called_once_with(**kwargs)
+
+            assert isinstance(
+                result, dict
+            ), f"Expected dict, but got {type(result).__name__}"
+            assert expected_keys.issubset(
+                result.keys()
+            ), f"Expected keys {expected_keys} but got {result.keys()}"
+            assert isinstance(
+                result.get(action), str
+            ), f"Expected int, but got {type(result.get(action)).__name__}"
+            assert result == {f"{action}": "performed a test action successfully"}
+    else:
+        with pytest.raises(ValueError, match=f"Action '{action}' is not supported."):
+            toolkit.get_tool_by_name(tool_name)(action=action, **kwargs)
---
+++ swarmauri_community/tests/tools/unit/GithubBranchTool_test.py
@@ -0,0 +1,114 @@
+import os
+from unittest.mock import MagicMock, patch
+from dotenv import load_dotenv
+
+import pytest
+from swarmauri_community.community.tools.concrete.GithubBranchTool import (
+    GithubBranchTool as Tool,
+)
+
+load_dotenv()
+
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not os.getenv("GITHUBTOOL_TEST_TOKEN"),
+    reason="Skipping due to environment variable not set",
+)
+def test_ubc_resource():
+    token = os.getenv("GITHUBTOOL_TEST_TOKEN")
+    tool = Tool(token=token)
+    assert tool.resource == "Tool"
+
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not os.getenv("GITHUBTOOL_TEST_TOKEN"),
+    reason="Skipping due to environment variable not set",
+)
+def test_ubc_type():
+    token = os.getenv("GITHUBTOOL_TEST_TOKEN")
+    assert Tool(token=token).type == "GithubBranchTool"
+
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not os.getenv("GITHUBTOOL_TEST_TOKEN"),
+    reason="Skipping due to environment variable not set",
+)
+def test_initialization():
+    token = os.getenv("GITHUBTOOL_TEST_TOKEN")
+    tool = Tool(token=token)
+    assert type(tool.id) == str
+
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not os.getenv("GITHUBTOOL_TEST_TOKEN"),
+    reason="Skipping due to environment variable not set",
+)
+def test_serialization():
+    token = os.getenv("GITHUBTOOL_TEST_TOKEN")
+    tool = Tool(token=token)
+    assert tool.id == Tool.model_validate_json(tool.model_dump_json()).id
+
+
+@pytest.mark.parametrize(
+    "action, kwargs, method_called",
+    [
+        (
+            "create_branch",
+            {
+                "repo_name": "test-repo",
+                "branch_name": "new-branch",
+                "source_branch": "main",
+            },
+            "create_branch",
+        ),
+        (
+            "delete_branch",
+            {"repo_name": "test-repo", "branch_name": "new-branch"},
+            "delete_branch",
+        ),
+        ("list_branches", {"repo_name": "test-repo"}, "list_branches"),
+        ("get_branch", {"repo_name": "test-repo", "branch_name": "main"}, "get_branch"),
+        # Invalid action
+        ("invalid_action", {}, None),
+    ],
+)
+@pytest.mark.skipif(
+    not os.getenv("GITHUBTOOL_TEST_TOKEN"),
+    reason="Skipping due to environment variable not set",
+)
+@pytest.mark.unit
+@patch("swarmauri_community.community.tools.concrete.GithubTool.Github")
+def test_call(mock_github, action, kwargs, method_called):
+    expected_keys = {action}
+    token = os.getenv("GITHUBTOOL_TEST_TOKEN")
+    tool = Tool(token=token)
+
+    mock_github.return_value = MagicMock()
+
+    if method_called is not None:
+        with patch.object(
+            Tool,
+            method_called,
+            return_value="performed a test action successfully",
+        ) as mock_method:
+            result = tool(action=action, **kwargs)
+
+            mock_method.assert_called_once_with(**kwargs)
+
+            assert isinstance(
+                result, dict
+            ), f"Expected dict, but got {type(result).__name__}"
+            assert expected_keys.issubset(
+                result.keys()
+            ), f"Expected keys {expected_keys} but got {result.keys()}"
+            assert isinstance(
+                result.get(action), str
+            ), f"Expected int, but got {type(result.get(action)).__name__}"
+            assert result == {f"{action}": "performed a test action successfully"}
+    else:
+        with pytest.raises(ValueError, match=f"Action '{action}' is not supported."):
+            tool(action=action, **kwargs)
