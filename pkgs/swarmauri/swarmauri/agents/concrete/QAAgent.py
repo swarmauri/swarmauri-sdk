2e01f0799fe9db2210a59460dbd302a604a058bb
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-09-21 14:24:20 -0500

    standard - Update QAAgent.py

--- pkgs/swarmauri/swarmauri/agents/concrete/QAAgent.py
+++ pkgs/swarmauri/swarmauri/agents/concrete/QAAgent.py
@@ -1,7 +1,7 @@
 from typing import Any, Optional, Dict, Literal
-from swarmauri.standard.agents.base.AgentBase import AgentBase
-from swarmauri.standard.conversations.concrete.MaxSystemContextConversation import MaxSystemContextConversation
-from swarmauri.standard.messages.concrete.HumanMessage import HumanMessage
+from swarmauri.agents.base.AgentBase import AgentBase
+from swarmauri.conversations.concrete.MaxSystemContextConversation import MaxSystemContextConversation
+from swarmauri.messages.concrete.HumanMessage import HumanMessage
 
 
 class QAAgent(AgentBase):
