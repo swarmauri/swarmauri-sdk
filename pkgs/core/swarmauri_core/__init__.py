8aed276d603d834cbf18d8d34b7e82fe5416d66e
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-09-21 14:55:10 -0500

    ☝️

--- pkgs/core/swarmauri_core/__init__.py
+++ pkgs/core/swarmauri_core/__init__.py
@@ -4,6 +4,7 @@ __long_desc__ = """
 
 ## Core 
 - **Core Interfaces**: Define the fundamental communication and data-sharing protocols between components in a Swarmauri-based system.
- 
 
+
+Visit us at: https://swarmauri.com
 """
