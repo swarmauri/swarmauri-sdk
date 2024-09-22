275066ad25520ea9b5dd8066f1def2b05565a7e1
RafaelJohn9 <rafaeljohb@gmail.com>
2024-09-22 10:35:36 +0300

    Refactor: Arranged tests according to unit, integration

---
+++ swarmauri/tests/integration/llms/CohereModel_test.py
@@ -0,0 +1,75 @@
+import pytest
+import os
+from swarmauri.standard.llms.concrete.CohereModel import CohereModel as LLM
+from swarmauri.standard.conversations.concrete.Conversation import Conversation
+
+from swarmauri.standard.messages.concrete.AgentMessage import AgentMessage
+from swarmauri.standard.messages.concrete.HumanMessage import HumanMessage
+from swarmauri.standard.messages.concrete.SystemMessage import SystemMessage
+
+
+@pytest.mark.acceptance
+@pytest.mark.skipif(
+    not os.getenv("COHERE_API_KEY"),
+    reason="Skipping due to environment variable not set",
+)
+def test_nonpreamble_system_context():
+    API_KEY = os.getenv("COHERE_API_KEY")
+    model = LLM(api_key=API_KEY)
+    conversation = Conversation()
+
+    # Say hi
+    input_data = "Hi"
+    human_message = HumanMessage(content=input_data)
+    conversation.add_message(human_message)
+
+    # Get Prediction
+    model.predict(conversation=conversation)
+
+    # Give System Context
+    system_context = 'You only respond with the following phrase, "Jeff"'
+    human_message = SystemMessage(content=system_context)
+    conversation.add_message(human_message)
+
+    # Prompt
+    input_data = "Hello Again."
+    human_message = HumanMessage(content=input_data)
+    conversation.add_message(human_message)
+
+    model.predict(conversation=conversation)
+    prediction = conversation.get_last().content
+    assert "Jeff" in prediction
+
+
+@pytest.mark.acceptance
+@pytest.mark.skipif(
+    not os.getenv("COHERE_API_KEY"),
+    reason="Skipping due to environment variable not set",
+)
+def test_multiple_system_contexts():
+    API_KEY = os.getenv("COHERE_API_KEY")
+    model = LLM(api_key=API_KEY)
+    conversation = Conversation()
+
+    system_context = 'You only respond with the following phrase, "Jeff"'
+    human_message = SystemMessage(content=system_context)
+    conversation.add_message(human_message)
+
+    input_data = "Hi"
+    human_message = HumanMessage(content=input_data)
+    conversation.add_message(human_message)
+
+    prediction = model.predict(conversation=conversation)
+
+    system_context_2 = 'You only respond with the following phrase, "Ben"'
+    human_message = SystemMessage(content=system_context_2)
+    conversation.add_message(human_message)
+
+    input_data_2 = "Hey"
+    human_message = HumanMessage(content=input_data_2)
+    conversation.add_message(human_message)
+
+    model.predict(conversation=conversation)
+    prediction = conversation.get_last().content
+    assert type(prediction) == str
+    assert "Ben" in prediction
