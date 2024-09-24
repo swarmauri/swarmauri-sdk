f8510048c16062ed2a944695da10a37f3af2831e
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-09-23 19:28:30 -0500

    experimental - ☝️ increment setup.py

--- pkgs/experimental/setup.py
+++ pkgs/experimental/setup.py
@@ -18,8 +18,8 @@ setup(
         "numpy",  # Common dependencies for all distributions
         "requests",
         "pydantic",
-        "swarmauri-core==0.5.0.dev7",
-        "swarmauri==0.5.0.dev7"
+        "swarmauri-core==0.5.0.dev8",
+        "swarmauri==0.5.0.dev8"
     ],
     extras_require={
         "full": [
