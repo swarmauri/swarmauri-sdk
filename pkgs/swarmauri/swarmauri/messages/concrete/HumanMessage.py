0fe4ee2990b6e56b654def6bc191d7e7a394bab9
ishaan1234 <31759888+ishaan1234@users.noreply.github.com>
2024-09-27 17:03:07 +0530

    Update HumanMessage.py

--- pkgs/swarmauri/swarmauri/messages/concrete/HumanMessage.py
+++ pkgs/swarmauri/swarmauri/messages/concrete/HumanMessage.py
@@ -1,9 +1,19 @@
-from typing import Optional, Any, Literal
-from pydantic import Field
+from typing import Optional, Any, Literal, List, Union, Dict
+from pydantic import BaseModel, Field
 from swarmauri.messages.base.MessageBase import MessageBase
 
+class TextContent(BaseModel):
+    type: Literal['text']
+    text: str
+
+class ImageURLContent(BaseModel):
+    type: Literal['image_url']
+    image_url: Dict[str, Any]
+
+ContentItem = Union[TextContent, ImageURLContent]
 class HumanMessage(MessageBase):
-    content: str
+    content: Union[str, List[ContentItem]]
     role: str = Field(default='user')
     name: Optional[str] = None
-    type: Literal['HumanMessage'] = 'HumanMessage'    
+    type: Literal['HumanMessage'] = 'HumanMessage'
+
