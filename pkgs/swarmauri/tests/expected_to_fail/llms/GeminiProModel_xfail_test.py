4f4349166b06cc9897b17766e4a656d907444b5b
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-09-27 13:40:24 +0100

    [swarmauri pkg]: updated the test expected to fail

--- pkgs/swarmauri/tests/expected_to_fail/llms/GeminiProModel_xfail_test.py
+++ pkgs/swarmauri/tests/expected_to_fail/llms/GeminiProModel_xfail_test.py
@@ -6,7 +6,6 @@ from swarmauri.conversations.concrete.Conversation import Conversation
 from swarmauri.messages.concrete.HumanMessage import HumanMessage
 from swarmauri.messages.concrete.SystemMessage import SystemMessage
 
-
 @pytest.fixture(scope="module")
 def gemini_pro_model():
     API_KEY = os.getenv("GEMINI_API_KEY")
@@ -15,7 +14,7 @@ def gemini_pro_model():
     llm = LLM(api_key=API_KEY)
     return llm
 
-
+@pytest.fail(reason="This test is expected to fail")
 @pytest.mark.acceptance
 def test_nonpreamble_system_context(gemini_pro_model):
     model = gemini_pro_model
@@ -42,3 +41,32 @@ def test_nonpreamble_system_context(gemini_pro_model):
     model.predict(conversation=conversation)
     prediction = conversation.get_last().content
     assert "Jeff" in prediction
+
+@pytest.fail(reason="This test is expected to fail")
+@pytest.mark.acceptance
+def test_multiple_system_contexts(gemini_pro_model):
+    model = gemini_pro_model
+    conversation = Conversation()
+
+    system_context = 'You only respond with the following phrase, "Jeff"'
+    human_message = SystemMessage(content=system_context)
+    conversation.add_message(human_message)
+
+    input_data = "Hi"
+    human_message = HumanMessage(content=input_data)
+    conversation.add_message(human_message)
+
+    prediction = model.predict(conversation=conversation)
+
+    system_context_2 = 'You only respond with the following phrase, "Ben"'
+    human_message = SystemMessage(content=system_context_2)
+    conversation.add_message(human_message)
+
+    input_data_2 = "Hey"
+    human_message = HumanMessage(content=input_data_2)
+    conversation.add_message(human_message)
+
+    model.predict(conversation=conversation)
+    prediction = conversation.get_last().content
+    assert type(prediction) == str
+    assert "Ben" in prediction
