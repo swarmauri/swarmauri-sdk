faf9fc56b82f6dc1cbc30c9c55db4c0103fc6e17
3rdSon <nnajivictorious@gmail.com>
2024-10-25 16:18:44 +0100

    swarm - groqaudio path

--- pkgs/swarmauri/tests/unit/llms/GroqAIAudio_unit_test.py
+++ pkgs/swarmauri/tests/unit/llms/GroqAIAudio_unit_test.py
@@ -4,7 +4,19 @@ import os
 from swarmauri.llms.concrete.GroqAIAudio import GroqAIAudio as LLM
 from swarmauri.utils.timeout_wrapper import timeout
 
+# Retrieve API key from environment variable
 API_KEY = os.getenv("GROQ_API_KEY")
+
+# Get the current working directory
+current_dir = os.getcwd()
+
+# Construct file paths dynamically
+file_path = os.path.join(
+    current_dir, "pkgs", "swarmauri", "tests", "static", "test.mp3"
+)
+file_path2 = os.path.join(
+    current_dir, "pkgs", "swarmauri", "tests", "static", "test_fr.mp3"
+)
 
 
 @pytest.fixture(scope="module")
@@ -54,7 +66,7 @@ def test_audio_transcription(groqai_model, model_name):
     model.name = model_name
 
     prediction = model.predict(
-        audio_path="pkgs/swarmauri/tests/static/test.mp3",
+        audio_path=file_path,
     )
 
     logging.info(prediction)
@@ -70,7 +82,7 @@ def test_audio_translation(groqai_model):
     model.name = "whisper-large-v3"
 
     prediction = model.predict(
-        audio_path="pkgs/swarmauri/tests/static/test_fr.mp3",
+        audio_path=file_path2,
         task="translation",
     )
 
