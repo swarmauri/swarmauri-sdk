4bd7ca98795bf656ada33fe1bf33af96f5765b5d
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-11-12 01:10:59 -0600

    cicd - Update conftest.py

--- pkgs/conftest.py
+++ pkgs/conftest.py
@@ -3,7 +3,7 @@ import os
 import inspect
 
 @pytest.hookimpl(tryfirst=True)
-def pytest_report_teststatus(report):
+def pytest_runtest_logreport(report):
     if report.failed:
-        return "F", "Custom failure message", "Test failed due to XYZ reason"
-    return report.status, report.shortrepr, report.longrepr
+        report.longrepr += "\nCustom Message: Something went wrong."
+    return report
