014a177c2f4e6502262d0df99c863ff89eb66099
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-11-12 01:10:01 -0600

    cicd - Update conftest.py

--- pkgs/conftest.py
+++ pkgs/conftest.py
@@ -2,15 +2,8 @@ import pytest
 import os
 import inspect
 
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    result = outcome.get_result()
-
-    if result.failed:
-        file_path = inspect.getfile(item.function)
-        line_number = inspect.getsourcelines(item.function)[1]
-
-        file_link = f"file://{os.path.abspath(file_path)}:{line_number}"
-
-        result.longrepr = f"{result.longrepr}\n\nCustom Error: View the failing test here -> {file_link}"
+@pytest.hookimpl(tryfirst=True)
+def pytest_report_teststatus(report):
+    if report.failed:
+        return "F", "Custom failure message", "Test failed due to XYZ reason"
+    return report.status, report.shortrepr, report.longrepr
