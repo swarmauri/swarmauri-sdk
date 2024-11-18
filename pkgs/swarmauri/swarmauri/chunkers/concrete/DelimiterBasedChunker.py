ce44d773ce0a0b5cdacb6fda97d90241f1bcc8af
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-11-18 17:55:59 -0600

    swarm - version DelimiterBasedChunker.py

--- pkgs/swarmauri/swarmauri/chunkers/concrete/DelimiterBasedChunker.py
+++ pkgs/swarmauri/swarmauri/chunkers/concrete/DelimiterBasedChunker.py
@@ -6,6 +6,7 @@ class DelimiterBasedChunker(ChunkerBase):
     """
     A concrete implementation of IChunker that splits text into chunks based on specified delimiters.
     """
+    version: str = "0.1.0.dev1"
     delimiters: List[str] = ['.', '!', '?']
     type: Literal['DelimiterBasedChunker'] = 'DelimiterBasedChunker'
     
