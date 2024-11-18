fda1c5315e29f6329f830a79de0a89640332fb0f
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-11-18 17:19:42 -0600

    Update FleschReadingEaseTool.py

--- pkgs/swarmauri/swarmauri/tools/concrete/FleschReadingEaseTool.py
+++ pkgs/swarmauri/swarmauri/tools/concrete/FleschReadingEaseTool.py
@@ -7,7 +7,7 @@ from swarmauri.tools.concrete.Parameter import Parameter
 
 
 class FleschReadingEaseTool(ToolBase):
-    version: str = "0.1.dev6"
+    version: str = "0.1.0.dev7"
     parameters: List[Parameter] = Field(
         default_factory=lambda: [
             Parameter(
