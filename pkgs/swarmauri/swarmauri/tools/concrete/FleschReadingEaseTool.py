70ca2a8456d1d9f300d859dde9380d59aee2afe4
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-11-18 17:17:30 -0600

    swarm - test bump FleschReadingEaseTool.py

--- pkgs/swarmauri/swarmauri/tools/concrete/FleschReadingEaseTool.py
+++ pkgs/swarmauri/swarmauri/tools/concrete/FleschReadingEaseTool.py
@@ -7,7 +7,7 @@ from swarmauri.tools.concrete.Parameter import Parameter
 
 
 class FleschReadingEaseTool(ToolBase):
-    version: str = "0.1.dev5"
+    version: str = "0.1.dev6"
     parameters: List[Parameter] = Field(
         default_factory=lambda: [
             Parameter(
