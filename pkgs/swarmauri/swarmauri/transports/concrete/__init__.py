6f2b7ddc3a9071971e296ffb10909538d3176279
MichaelDecent <michaeldecent2@gmail.com>
2024-12-19 14:35:23 +0100

    Refactor lazy loading implementation to use LazyLoader across multiple modules

--- pkgs/swarmauri/swarmauri/transports/concrete/__init__.py
+++ pkgs/swarmauri/swarmauri/transports/concrete/__init__.py
@@ -1,4 +1,4 @@
-from swarmauri.utils._lazy_import import _lazy_import
+from swarmauri.utils.LazyLoader import LazyLoader
 
 # List of transport names (file names without the ".py" extension) and corresponding class names
 transport_files = [
@@ -7,7 +7,7 @@ transport_files = [
 
 # Lazy loading of transport classes, storing them in variables
 for module_name, class_name in transport_files:
-    globals()[class_name] = _lazy_import(module_name, class_name)
+    globals()[class_name] = LazyLoader(module_name, class_name)
 
 # Adding the lazy-loaded transport classes to __all__
 __all__ = [class_name for _, class_name in transport_files]
