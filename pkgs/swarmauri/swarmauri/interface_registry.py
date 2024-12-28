4140395434a24c0cd498c2bbdd9f046f7d96efda
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-12-28 15:20:53 -0600

    Update interface_registry.py

--- pkgs/swarmauri/swarmauri/interface_registry.py
+++ pkgs/swarmauri/swarmauri/interface_registry.py
@@ -13,12 +13,38 @@ from swarmauri_base.chunkers.ChunkerBase import ChunkerBase
 # Define the mapping
 INTERFACE_REGISTRY = {
     "swarmauri.agents": AgentBase,
-    #"swarmauri.chains": ChainBase,
+    "swarmauri.chains": ChainBase,
     "swarmauri.chunkers": ChunkerBase,
-    #"swarmauri.conversations": ConversationBase,
-    #"swarmauri.llms": LLMBase,
-    "swarmauri.utils": None
+    "swarmauri.control_panels": ControlPanelBase,
+    "swarmauri.conversations": ConversationBase,
+    "swarmauri.dataconnectors": DataConnectorBase,
+    "swarmauri.distances": DistanceBase,
+    "swarmauri.documents": DocumentBase,
+    "swarmauri.embeddings": EmbeddingBase,
+    "swarmauri.exceptions": ExceptionBase,
+    "swarmauri.factories": FactoryBase,
+    "swarmauri.image_gens": ImageGenBase,
+    "swarmauri.llms": LLMBase,
+    "swarmauri.measurements": MeasurementBase,
+    "swarmauri.messages": MessageBase,
+    "swarmauri.parsers": ParserBase,
+    "swarmauri.pipelines": PipelineBase,
+    "swarmauri.prompts": PromptBase,
+    "swarmauri.plugins": None,
+    "swarmauri.schema_converters": SchemaConverterBase,
+    "swarmauri.service_registries": ServiceRegistryBase,
+    "swarmauri.state": StateBase,
+    "swarmauri.swarms": SwarmBase,
+    "swarmauri.task_mgt_strategies": TaskMgtStrategyBase,
+    "swarmauri.toolkits": ToolkitBase,
+    "swarmauri.tools": ToolBase,
+    "swarmauri.tracing": None,
+    "swarmauri.transports": TransportBase,
+    "swarmauri.utils": None,
+    "swarmauri.vector_stores": VectorStoreBase,
+    "swarmauri.vectors": VectorBase,
 }
+
 
 def get_interface_for_resource(resource_kind):
     """
