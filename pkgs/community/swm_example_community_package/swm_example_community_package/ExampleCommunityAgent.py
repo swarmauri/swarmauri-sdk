2df41d0b427d737c037325c26ab95fd58e54ce7e
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-01-04 07:40:36 -0600

    external - update paths

--- pkgs/community/swm_example_community_package/swm_example_community_package/ExampleCommunityAgent.py
+++ pkgs/community/swm_example_community_package/swm_example_community_package/ExampleCommunityAgent.py
@@ -1,7 +1,7 @@
 from typing import Any, Optional, Dict, Literal
 from swarmauri_base.agents.AgentBase import AgentBase
 from swarmauri_base.conversations.ConversationBase import ConversationBase
-from swarmauri_core.typing import SubclassUnion
+from swarmauri_core.ComponentBase import SubclassUnion
 
 class ExampleCommunityAgent(AgentBase):
     conversation: SubclassUnion[ConversationBase]
