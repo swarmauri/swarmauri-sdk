77e91f16f0fc4a209975d63a7a03c3abacb5856b
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-01-04 14:17:15 -0600

    standard - test

--- pkgs/standards/swarmauri_standard/swarmauri_standard/agents/QAAgent.py
+++ pkgs/standards/swarmauri_standard/swarmauri_standard/agents/QAAgent.py
@@ -9,7 +9,7 @@ from swarmauri_core.ComponentBase import SubclassUnion, ComponentBase
 
 @ComponentBase.register_type(AgentBase, 'QAAgent')
 class QAAgent(AgentBase):
-    llm: SubclassUnion[LLMBase]
+    #llm: SubclassUnion[LLMBase]
     conversation: MaxSystemContextConversation = MaxSystemContextConversation(max_size=2)
     type: Literal['QAAgent'] = 'QAAgent'
 
