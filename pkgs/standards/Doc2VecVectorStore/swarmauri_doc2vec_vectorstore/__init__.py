f21dcfbfb22f6d7faa81d1770b14712149533b2c
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-01-06 23:01:24 -0600

    standards - implement Doc2VecStore 1st class plugin

--- pkgs/standards/Doc2VecVectorStore/swarmauri_doc2vec_vectorstore/__init__.py
+++ pkgs/standards/Doc2VecVectorStore/swarmauri_doc2vec_vectorstore/__init__.py
@@ -1,10 +1,13 @@
-from .ExampleAgent import ExampleAgent
+from .Doc2VecEmbedding import Doc2VecEmbedding
+from .Doc2VecVectorStore import Doc2VecVectorStore
 __version__ = "0.6.0.dev26"
 __long_desc__ = """
 
-# Swarmauri Example Plugin
+# Swarmauri Doc2Vec Based Components
 
-This repository includes an example of a Swarmauri Plugin.
+Components Included:
+- Doc2VecEmbedding
+- Doc2VecVectorStore
 
 Visit us at: https://swarmauri.com
 Follow us at: https://github.com/swarmauri
