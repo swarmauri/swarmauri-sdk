bd63e25ad0df5588745e0cb06cc4d35c186dedcf
MichaelDecent <michaeldecent2@gmail.com>
2025-01-09 11:20:47 +0100

    feat: add PersistentChromaDBVectorStore plugin with implementation and unit tests

---
+++ pkgs/community/swarmauri_vectorstore_communitypersistentchromaDB/swarmauri_vectorstore_communitypersistentchromaDB/__init__.py
@@ -0,0 +1,14 @@
+from .PersistentChromaDBVectorStore import PersistentChromaDBVectorStore
+
+__version__ = "0.6.0.dev26"
+__long_desc__ = """
+
+# Swarmauri PersistentChromaDB Plugin
+
+This repository includes an PersistentChromaDB of a Swarmauri Plugin.
+
+Visit us at: https://swarmauri.com
+Follow us at: https://github.com/swarmauri
+Star us at: https://github.com/swarmauri/swarmauri-sdk
+
+"""
