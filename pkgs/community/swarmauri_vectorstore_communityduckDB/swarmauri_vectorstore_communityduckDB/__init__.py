71bd52a4d8e7ef22d9b68458e7cb0838bb453f10
MichaelDecent <michaeldecent2@gmail.com>
2025-01-09 11:58:37 +0100

    feat: add Swarmauri DuckDB Vector Store package with README and configuration

---
+++ pkgs/community/swarmauri_vectorstore_communityduckDB/swarmauri_vectorstore_communityduckDB/__init__.py
@@ -0,0 +1,14 @@
+from .DuckDBVectorStore import DuckDBVectorStore
+
+__version__ = "0.6.0.dev26"
+__long_desc__ = """
+
+# Swarmauri DuckDB Plugin
+
+This repository includes a DuckDB of a Swarmauri Plugin.
+
+Visit us at: https://swarmauri.com
+Follow us at: https://github.com/swarmauri
+Star us at: https://github.com/swarmauri/swarmauri-sdk
+
+"""
