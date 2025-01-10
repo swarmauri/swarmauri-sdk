3b6e3533e2a495aa357a059e4644f642ff7f0c40
MichaelDecent <michaeldecent2@gmail.com>
2025-01-10 12:50:58 +0100

    feat: add Swarmauri Psutil Tool package with README, configuration, and unit tests

---
+++ pkgs/community/swarmauri_tool_communitypsutil/swarmauri_tool_communitypsutil/__init__.py
@@ -0,0 +1,15 @@
+from .PsutilTool import PsutilTool
+__version__ = "0.6.0.dev26"
+__long_desc__ = """
+
+# Swarmauri Psutil Plugin
+
+This repository includes a Psutil of a Swarmauri Plugin.
+
+Visit us at: https://swarmauri.com
+Follow us at: https://github.com/swarmauri
+Star us at: https://github.com/swarmauri/swarmauri-sdk
+
+"""
+
+
