240ed227a3855335b4dec8e5faf1cb88ac4c69a6
MichaelDecent <michaeldecent2@gmail.com>
2025-01-13 09:32:24 +0100

    Add Swarmauri Annoy Vector Store community package

---
+++ pkgs/community/swarmauri_vectorstore_communityannoy/swarmauri_vectorstore_communityannoy/__init__.py
@@ -0,0 +1,12 @@
+from .AnnoyVectorStore import AnnoyVectorStore
+
+__version__ = "0.6.0.dev26"
+__long_desc__ = """
+
+# Swarmauri Annoy vector store Plugin
+
+Visit us at: https://swarmauri.com
+Follow us at: https://github.com/swarmauri
+Star us at: https://github.com/swarmauri/swarmauri-sdk
+
+"""
