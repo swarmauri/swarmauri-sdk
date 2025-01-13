6e4d5662039bc2f0fa3a2f026b76c913d0402f83
MichaelDecent <michaeldecent2@gmail.com>
2025-01-13 09:47:51 +0100

    Add Swarmauri CloudQdrant VectorStore community package with tests

---
+++ pkgs/community/swarmauri_vectorstore_communitycloudqdrant/swarmauri_vectorstore_communitycloudqdrant/__init__.py
@@ -0,0 +1,12 @@
+from .CloudQdrantVectorStore import CloudQdrantVectorStore
+
+__version__ = "0.6.0.dev26"
+__long_desc__ = """
+
+# Swarmauri CloudQdrant VectorStore Plugin
+
+Visit us at: https://swarmauri.com
+Follow us at: https://github.com/swarmauri
+Star us at: https://github.com/swarmauri/swarmauri-sdk
+
+"""
