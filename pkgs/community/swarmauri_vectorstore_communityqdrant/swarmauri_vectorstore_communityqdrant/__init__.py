324e6151f2bbc03fa8d79698e2c23292d4192583
MichaelDecent <michaeldecent2@gmail.com>
2025-01-13 11:11:06 +0100

    Add Swarmauri Persistent Qdrant Vector Store community package with tests

--- pkgs/community/swarmauri_vectorstore_communityqdrant/swarmauri_vectorstore_communityqdrant/__init__.py
+++ pkgs/community/swarmauri_vectorstore_communityqdrant/swarmauri_vectorstore_communityqdrant/__init__.py
@@ -1,9 +1,15 @@
+from .PersistentQdrantVectorStore import PersistentQdrantVectorStore
 from .CloudQdrantVectorStore import CloudQdrantVectorStore
 
 __version__ = "0.6.0.dev26"
 __long_desc__ = """
 
-# Swarmauri CloudQdrant VectorStore Plugin
+# Swarmauri Qdrant Based Components
+
+Components Included:
+- PersistentQdrantVectorStore
+- CloudQdrantVectorStore
+
 
 Visit us at: https://swarmauri.com
 Follow us at: https://github.com/swarmauri
