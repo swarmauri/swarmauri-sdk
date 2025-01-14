58f440aa75b45c2a1e82eea88e69fae4a283063a
MichaelDecent <michaeldecent2@gmail.com>
2025-01-14 01:20:35 +0100

    Add Swarmauri PytesseractImg2Text community package

---
+++ pkgs/community/swarmauri_llm_communitypytesseractImg2text/swarmauri_llm_communitypytesseractImg2text/__init__.py
@@ -0,0 +1,12 @@
+from .PytesseractImg2TextModel import PytesseractImg2TextModel
+
+__version__ = "0.6.0.dev26"
+__long_desc__ = """
+
+# Swarmauri PytesseractImg2Text Model Plugin
+
+Visit us at: https://swarmauri.com
+Follow us at: https://github.com/swarmauri
+Star us at: https://github.com/swarmauri/swarmauri-sdk
+
+"""
