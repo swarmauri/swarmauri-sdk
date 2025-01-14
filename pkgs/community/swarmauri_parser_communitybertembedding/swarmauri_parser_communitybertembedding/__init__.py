92ce44d6eef650097404ed0d221cc90365c55229
MichaelDecent <michaeldecent2@gmail.com>
2025-01-14 01:46:17 +0100

    Add Swarmauri BERTEmbedding Parser community package

---
+++ pkgs/community/swarmauri_parser_communitybertembedding/swarmauri_parser_communitybertembedding/__init__.py
@@ -0,0 +1,12 @@
+from .BERTEmbeddingParser import BERTEmbeddingParser
+
+__version__ = "0.6.0.dev26"
+__long_desc__ = """
+
+# Swarmauri BERTEmbedding Parser Plugin
+
+Visit us at: https://swarmauri.com
+Follow us at: https://github.com/swarmauri
+Star us at: https://github.com/swarmauri/swarmauri-sdk
+
+"""
