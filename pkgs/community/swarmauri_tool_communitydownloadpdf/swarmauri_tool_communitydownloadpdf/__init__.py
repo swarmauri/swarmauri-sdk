aafecb47d832153080db4bd0c43f87bdc73b5346
MichaelDecent <michaeldecent2@gmail.com>
2025-01-15 01:27:48 +0100

    Add Swarmauri community packages for Gmail, PDF download, Captcha generation, and entity recognition

---
+++ pkgs/community/swarmauri_tool_communitydownloadpdf/swarmauri_tool_communitydownloadpdf/__init__.py
@@ -0,0 +1,16 @@
+from .DownloadPdfTool import DownloadPDFTool as Tool
+
+__version__ = "0.6.0.dev26"
+__long_desc__ = """
+
+# Swarmauri DownloadPdf Tool Plugin
+
+This repository includes an example of a Swarmauri Plugin.
+
+Visit us at: https://swarmauri.com
+Follow us at: https://github.com/swarmauri
+Star us at: https://github.com/swarmauri/swarmauri-sdk
+
+"""
+
+
---
+++ pkgs/community/swarmauri_tool_communitygmail/swarmauri_tool_communitygmail/__init__.py
@@ -0,0 +1,17 @@
+from .GmailReadTool import GmailReadTool
+from .GmailSendTool import GmailSendTool
+
+__version__ = "0.6.0.dev26"
+__long_desc__ = """
+
+# Swarmauri Gmail Plugin
+
+Components Included:
+- GmailSendTool
+- GmailReadTool
+
+Visit us at: https://swarmauri.com
+Follow us at: https://github.com/swarmauri
+Star us at: https://github.com/swarmauri/swarmauri-sdk
+
+"""
