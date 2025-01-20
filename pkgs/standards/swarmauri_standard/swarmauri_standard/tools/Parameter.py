2d2d36bb5acd229f920311c8ec6debafc58af6a8
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-01-20 01:02:37 -0600

    standard - fix circular import

--- pkgs/standards/swarmauri_standard/swarmauri_standard/tools/Parameter.py
+++ pkgs/standards/swarmauri_standard/swarmauri_standard/tools/Parameter.py
@@ -1,6 +1,5 @@
 from typing import List, Literal, Union
 from swarmauri_base.tools.ParameterBase import ParameterBase
-from swarmauri_standard.tools.Parameter import Parameter
 from swarmauri_base.tools.ToolBase import ToolBase
 from swarmauri_core.ComponentBase import ComponentBase
 
