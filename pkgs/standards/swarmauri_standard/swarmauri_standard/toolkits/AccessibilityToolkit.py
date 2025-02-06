15bb5bb53b36a15e2bdfffa662a85bf5b76f9b71
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-02-06 10:23:33 -0600

    standards - update toolkit

--- pkgs/standards/swarmauri_standard/swarmauri_standard/toolkits/AccessibilityToolkit.py
+++ pkgs/standards/swarmauri_standard/swarmauri_standard/toolkits/AccessibilityToolkit.py
@@ -4,16 +4,6 @@ from pydantic import Field
 from swarmauri_base.toolkits.ToolkitBase import ToolkitBase
 
 from swarmauri_standard.tools.AutomatedReadabilityIndexTool import (
-<<<<<<< Updated upstream
-    AutomatedReadabilityIndexTool,
-)
-from swarmauri_standard.tools.ColemanLiauIndexTool import ColemanLiauIndexTool
-from swarmauri_standard.tools.FleschKincaidTool import FleschKincaidTool
-from swarmauri_standard.tools.FleschReadingEaseTool import (
-    FleschReadingEaseTool,
-)
-from swarmauri_standard.tools.GunningFogTool import GunningFogTool
-=======
     AutomatedReadabilityIndexTool
 )
 from swarmauri_standard.tools.ColemanLiauIndexTool import (
@@ -27,12 +17,10 @@ from swarmauri_standard.tools.FleschKincaidTool import (
 from swarmauri_standard.tools.FleschReadingEaseTool import (
     GunningFogTool
     )
->>>>>>> Stashed changes
 
 from swarmauri_core.ComponentBase import ComponentBase
 
-
-@ComponentBase.register_type(ToolkitBase, "AccessibilityToolkit")
+@ComponentBase.register_type(ToolkitBase, 'AccessibilityToolkit')
 class AccessibilityToolkit(ToolkitBase):
     type: Literal["AccessibilityToolkit"] = "AccessibilityToolkit"
     tools: Dict[str, SubclassUnion[ToolBase]] = {
