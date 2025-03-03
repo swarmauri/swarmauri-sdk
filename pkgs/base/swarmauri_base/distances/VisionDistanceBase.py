5407e8cd4ca81c27eac53554f816dec18260f6d3
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-03-03 14:31:34 -0600

    bae - sanitize

--- pkgs/base/swarmauri_base/distances/VisionDistanceBase.py
+++ pkgs/base/swarmauri_base/distances/VisionDistanceBase.py
@@ -3,7 +3,6 @@ from pydantic import Field
 
 from swarmauri_base.ComponentBase import ComponentBase, ResourceTypes
 
-
 class VisionDistanceBase(ComponentBase):
     resource: Optional[str] = Field(default=ResourceTypes.DISTANCE.value, frozen=True)
     type: Literal["VisionDistanceBase"] = "VisionDistanceBase"
