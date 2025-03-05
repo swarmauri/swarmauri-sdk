ceb6946d3fb4a7a75d778f4ec9dcf1c717e20a01
RafaelJohn9 <rafaeljohb@gmail.com>
2025-03-05 16:54:03 +0300

    Feature: Swarmauri Prompt Template
    Signed-off-by: RafaelJohn9 <rafaeljohb@gmail.com>

---
+++ pkgs/standards/swarmauri_prompt_j2prompttemplate/tests/unit/test_J2PromptTemplate.py
@@ -0,0 +1,80 @@
+import os
+from tempfile import NamedTemporaryFile
+import pytest
+from jinja2 import Template
+from swarmauri_prompt_j2prompttemplate.J2PromptTemplate import J2PromptTemplate
+
+
+@pytest.mark.unit
+def test_j2prompt_template_type():
+    assert J2PromptTemplate().type == "J2PromptTemplate"
+
+
+@pytest.mark.unit
+def test_initialization():
+    template = J2PromptTemplate()
+    assert isinstance(template.template, str)
+    assert isinstance(template.variables, dict)
+
+
+@pytest.mark.unit
+def test_set_template_from_string():
+    template = J2PromptTemplate()
+    template_str = "Hello, {{ name }}!"
+    template.set_template(template_str)
+    assert template.template == template_str
+
+
+@pytest.mark.unit
+def test_set_template_from_filepath():
+    template = J2PromptTemplate()
+    with NamedTemporaryFile(delete=False, suffix=".j2") as temp_file:
+        temp_file.write(b"Hello, {{ name }}!")
+        temp_file_path = temp_file.name
+
+    template.set_template(temp_file_path)
+    assert isinstance(template.template, Template)
+    os.remove(temp_file_path)
+
+
+@pytest.mark.unit
+def test_generate_prompt_from_string_template():
+    template = J2PromptTemplate()
+    template_str = "Hello, {{ name }}!"
+    template.set_template(template_str)
+    result = template.generate_prompt({"name": "World"})
+    assert result == "Hello, World!"
+
+
+@pytest.mark.unit
+def test_generate_prompt_from_file_template():
+    template = J2PromptTemplate()
+    with NamedTemporaryFile(delete=False, suffix=".j2") as temp_file:
+        temp_file.write(b"Hello, {{ name }}!")
+        temp_file_path = temp_file.name
+
+    template.set_template(temp_file_path)
+    result = template.generate_prompt({"name": "World"})
+    assert result == "Hello, World!"
+    os.remove(temp_file_path)
+
+
+@pytest.mark.unit
+def test_fill_with_variables():
+    template = J2PromptTemplate()
+    template_str = "Hello, {{ name }}!"
+    template.set_template(template_str)
+    result = template.fill({"name": "World"})
+    assert result == "Hello, World!"
+
+
+@pytest.mark.unit
+def test_split_whitespace():
+    result = J2PromptTemplate.split_whitespace("Hello World")
+    assert result == ["Hello", "World"]
+
+    result = J2PromptTemplate.split_whitespace("Hello,World", delimiter=",")
+    assert result == ["Hello", "World"]
+
+    with pytest.raises(ValueError):
+        J2PromptTemplate.split_whitespace(123)
