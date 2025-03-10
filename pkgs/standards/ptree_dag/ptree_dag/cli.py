605cf2d8ff5aa782ab20b7c0b7191be3f39c5dae
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-03-10 16:04:48 -0500

    chore(standards): add banner; default trunc

--- pkgs/standards/ptree_dag/ptree_dag/cli.py
+++ pkgs/standards/ptree_dag/ptree_dag/cli.py
@@ -11,8 +11,12 @@ import pkgutil
 from pydantic import FilePath
 from .core import ProjectFileGenerator, Fore, Back, Style
 from ._config import _config
-
+from ._banner import _print_banner
 app = typer.Typer(help="CLI tool for processing project files using ProjectFileGenerator.")
+
+
+_print_banner()
+
 
 @app.command("process")
 def process(
@@ -22,7 +26,7 @@ def process(
     additional_package_dirs: str = typer.Option(None, help="Optional list of additional directories to include in J2 env. Delimited by ','"),
     provider: str = typer.Option(None, help="The LLM Provider (DeepInfra, LlamaCpp, Openai)"),
     model_name: str = typer.Option(None, help="The model_name to use."),
-    trunc: bool = typer.Option(False, help="Truncate response (True or False)"),
+    trunc: bool = typer.Option(True, help="Truncate response (True or False)"),
     start_idx: int = typer.Option(0, help="Start at a certain file (Use sort to find idx number)"),
     start_file: str = typer.Option(None, help="Start at a certain file name-wise (Use sort to find filename)")
 ):
@@ -40,27 +44,31 @@ def process(
     # Set Response in config
     _config["truncate"] = trunc
 
-    pfg = ProjectFileGenerator(
-        projects_payload_path=str(projects_payload),
-        template_base_dir=str(template_base_dir) if template_base_dir else None,
-        additional_package_dirs=additional_package_dirs,
-        agent_env={"provider": provider, "model_name": model_name}
-    )
-    if project_name:
-        projects = pfg.load_projects()
-        # Look for a project with the matching "PROJECT_NAME" key
-        project = next((proj for proj in projects if proj.get("PROJECT_NAME") == project_name), None)
-        if project is None:
-            pfg.logger.info(f"Project '{project_name}' not found.", err=True)
-            raise typer.Exit(code=1)
-        if start_file:
-            sorted_records, start_idx = pfg.process_single_project(project, start_file=start_file)
+    try:
+        pfg = ProjectFileGenerator(
+            projects_payload_path=str(projects_payload),
+            template_base_dir=str(template_base_dir) if template_base_dir else None,
+            additional_package_dirs=additional_package_dirs,
+            agent_env={"provider": provider, "model_name": model_name}
+        )
+        if project_name:
+            projects = pfg.load_projects()
+            # Look for a project with the matching "PROJECT_NAME" key
+            project = next((proj for proj in projects if proj.get("PROJECT_NAME") == project_name), None)
+            if project is None:
+                pfg.logger.info(f"Project '{project_name}' not found.", err=True)
+                raise typer.Exit(code=1)
+            if start_file:
+                sorted_records, start_idx = pfg.process_single_project(project, start_file=start_file)
+            else:
+                sorted_records, start_idx = pfg.process_single_project(project, start_idx=start_idx if start_idx else 0)
+            pfg.logger.info(f"Processed project '{project_name}' successfully.")
         else:
-            sorted_records, start_idx = pfg.process_single_project(project, start_idx=start_idx if start_idx else 0)
-        pfg.logger.info(f"Processed project '{project_name}' successfully.")
-    else:
-        pfg.process_all_projects()
-        pfg.logger.info("Processed all projects successfully.")
+            pfg.process_all_projects()
+            pfg.logger.info("Processed all projects successfully.")
+    except KeyboardInterrupt:
+        typer.echo("\n  Interrupted... exited.")
+        typer.Exit(code=1)
 
 
 @app.command("sort")
@@ -71,7 +79,6 @@ def process(
     additional_package_dirs: str = typer.Option(None, help="Optional list of additional directories to include in J2 env. Delimited by ','"),
     provider: str = typer.Option(None, help="The LLM Provider (DeepInfra, LlamaCpp, Openai)"),
     model_name: str = typer.Option(None, help="The model_name to use."),
-    trunc: bool = typer.Option(False, help="Truncate response (True or False)"),
     start_idx: int = typer.Option(None, help="Start at a certain file idx-wise (Use sort to find idx number)"),
     start_file: str = typer.Option(None, help="Start at a certain file name-wise (Use sort to find filename)")
 ):
@@ -83,11 +90,9 @@ def process(
         pfg.logger.error(f"Cannot assign both start_idx and start_file.")
         raise typer.Exit(code=1)
 
+
     additional_package_dirs = additional_package_dirs.split(',') if additional_package_dirs else []
     additional_package_dirs = [FilePath(_d) for _d in additional_package_dirs]
-
-    # Set Response in config
-    _config["truncate"] = trunc
 
     pfg = ProjectFileGenerator(
         projects_payload_path=str(projects_payload),
@@ -115,8 +120,7 @@ def process(
         
     else:
         projects_sorted_records = pfg.process_all_projects()
-        
-        for sorted_records in projects_sorted_records: 
+        for sorted_records in projects_sorted_records:
             project_name = sorted_records[0].get("PROJECT_NAME")
             pfg.logger.info("")
             pfg.logger.info(Fore.GREEN + f"{[project_name]}" + Style.RESET_ALL)
