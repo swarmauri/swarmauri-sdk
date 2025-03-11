9f404e7b8b7e7b8a4cd13014aa6624c6e5778ba2
MichaelDecent <michaeldecent2@gmail.com>
2025-03-11 14:41:40 +0100

    feat(logging): implement configurable log formatter with optional timestamp, process, and thread details

--- pkgs/swarmauri_standard/swarmauri_standard/logger_formatters/LoggerFormatter.py
+++ pkgs/swarmauri_standard/swarmauri_standard/logger_formatters/LoggerFormatter.py
@@ -5,4 +5,27 @@ from swarmauri_base.logging.FormatterBase import FormatterBase
 @register_type()
 class LoggerFormatter(FormatterBase):
     """Concrete implementation of FormatterBase"""
-    pass
+
+    include_timestamp: bool = False
+    include_process: bool = False
+    include_thread: bool = False
+    date_format: str = "%Y-%m-%d %H:%M:%S"
+
+    def model_post_init(self, *args, **kwargs):
+        format_parts = []
+
+        if self.include_timestamp:
+            format_parts.append("%(asctime)s")
+
+        format_parts.append("[%(name)s]")
+        format_parts.append("[%(levelname)s]")
+
+        if self.include_process:
+            format_parts.append("Process:%(process)d")
+
+        if self.include_thread:
+            format_parts.append("Thread:%(thread)d")
+
+        format_parts.append("%(message)s")
+
+        self.format = " ".join(format_parts)
