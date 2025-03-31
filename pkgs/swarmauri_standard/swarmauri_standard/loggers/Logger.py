12fa512379d4158601683aae4c3913bb86297922
MichaelDecent <michaeldecent2@gmail.com>
2025-03-31 08:38:13 +0100

    feat(logging): fix logger file path

--- pkgs/swarmauri_standard/swarmauri_standard/loggers/Logger.py
+++ pkgs/swarmauri_standard/swarmauri_standard/loggers/Logger.py
@@ -1,8 +1,9 @@
-from swarmauri_base.loggers.LoggerBase import LoggerBase
 from swarmauri_base import register_type
+from swarmauri_base.loggers.LoggerBase import LoggerBase
 
 
 @register_type()
 class Logger(LoggerBase):
     """Concrete implementation of LoggerBase"""
+
     pass
