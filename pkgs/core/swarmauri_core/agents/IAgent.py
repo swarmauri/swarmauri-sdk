464cac105ad5f49d27c4e45a0b701f3d4a4d2912
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-05-03 13:42:30 -0500

    fix(core): fix NameError

--- pkgs/core/swarmauri_core/agents/IAgent.py
+++ pkgs/core/swarmauri_core/agents/IAgent.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Optional, Dict, List
+from typing import Any, Optional, Dict, List, Union
 from swarmauri_core.messages.IMessage import IMessage
 
 
