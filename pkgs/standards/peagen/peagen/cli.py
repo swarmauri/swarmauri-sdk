fe63ad6cc89b9f00ed2af1514a59d08883b9fa88
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-05-18 05:10:48 -0500

    feat(peagen): prototype `peagen init`

--- pkgs/standards/peagen/peagen/cli.py
+++ pkgs/standards/peagen/peagen/cli.py
@@ -3,6 +3,7 @@ import typer
 
 from peagen._banner import _print_banner
 from peagen.commands import (
+    init_app,
     process_app,
     revise_app,
     sort_app,
@@ -14,6 +15,7 @@ _print_banner()
 
 app = typer.Typer(help="CLI tool for processing project files using Peagen.")
 
+app.add_typer(init_app, name="init")
 app.add_typer(process_app)
 app.add_typer(revise_app)
 app.add_typer(sort_app)
