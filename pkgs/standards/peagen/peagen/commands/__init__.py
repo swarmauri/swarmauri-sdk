0fb42b72f2afd00b888bf3f1741001124a6cd171
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-05-18 05:10:48 -0500

    feat(peagen): prototype `peagen init`

--- pkgs/standards/peagen/peagen/commands/__init__.py
+++ pkgs/standards/peagen/peagen/commands/__init__.py
@@ -1,3 +1,4 @@
+from peagen.commands.init import init_app
 from peagen.commands.process import process_app
 from peagen.commands.revise import revise_app
 from peagen.commands.sort import sort_app
@@ -5,6 +6,7 @@ from peagen.commands.templates import template_sets_app
 from peagen.commands.doe import doe_app
 
 __all__ = [
+    "init_app",
     "process_app",
     "revise_app",
     "sort_app",
