dae11e748fad76b1abe540e32678932b0f4c2260
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-02 07:46:37 -0500

    fix(peagen): add gate args

--- pkgs/standards/peagen/gateway/scripts/dev_worker.py
+++ pkgs/standards/peagen/gateway/scripts/dev_worker.py
@@ -2,10 +2,12 @@ import os, argparse, uvicorn
 
 def main():
     ap = argparse.ArgumentParser()
+    ap.add_argument("--gw", default="http://localhost:8000/rpc")
     ap.add_argument("--pool", default="default")
     ap.add_argument("--port", default=9001, type=int)
     args = ap.parse_args()
 
+    os.environ["DQ_GATEWAY"] = args.gw
     os.environ["DQ_POOL"] = args.pool
     os.environ["PORT"]   = str(args.port)
 
