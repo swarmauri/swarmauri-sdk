ba8a32a313f38af75cf1f1a800223432c876776a
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-03 14:27:42 -0500

    fix(peagen): remove prog, add fetch

--- pkgs/standards/peagen/peagen/cli/commands/__init__.py
+++ pkgs/standards/peagen/peagen/cli/commands/__init__.py
@@ -1,23 +1,23 @@
 """Expose all Typer sub-applications for the CLI."""
 
+from .doe import doe_app
+from .eval import eval_app
+from .extras import extras_app
+from .fetch import fetch_app
 from .init import init_app
 from .process import process_app
 from .sort import sort_app
 from .templates import template_sets_app
-from .doe import doe_app
-from .program import program_app
 from .validate import validate_app
-from .extras import extras_app
-from .eval import eval_app
 
 __all__ = [
+    "doe_app",
+    "eval_app",
+    "extras_app",
+    "fetch_app",
     "init_app",
     "process_app",
     "sort_app",
     "template_sets_app",
-    "doe_app",
-    "program_app",
     "validate_app",
-    "extras_app",
-    "eval_app",
 ]
