145d44ab6cbed556f8ab419fee806a4bad369596
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-11 01:43:14 -0500

    fix(peagen): fix NameError

--- pkgs/standards/peagen/migrations/versions/ae1de73e4143_init.py
+++ pkgs/standards/peagen/migrations/versions/ae1de73e4143_init.py
@@ -2,7 +2,7 @@
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy import inspect
-from peagen.db.enums import status_enum          # create_type=False
+from peagen.models.task_run import status_enum      # ← shared object, create_type=False
 
 revision = "ae1de73e4143"
 down_revision = None
