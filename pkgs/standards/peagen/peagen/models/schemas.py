befc16e3a27f7a21a59fe5f4d59126da49a8756e
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-11 00:21:58 -0500

    fix(peagen): fix NameError

--- pkgs/standards/peagen/peagen/models/schemas.py
+++ pkgs/standards/peagen/peagen/models/schemas.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from enum import Enum
 from typing import List, Optional
 from pydantic import BaseModel, Field
+from sqlalchemy.dialects import postgresql as psql
 import uuid
 
 
