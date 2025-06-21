326398cc639ec6203d469338168334e2264892ef
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-21 04:15:17 -0500

    Add PytestEvaluator for peagen

--- pkgs/standards/peagen/peagen/plugins/evaluators/__init__.py
+++ pkgs/standards/peagen/peagen/plugins/evaluators/__init__.py
@@ -1,4 +1,5 @@
 from .performance_evaluator import PerformanceEvaluator
+from .pytest_evaluator import PytestEvaluator
 
-__all__ = ["PerformanceEvaluator"]
+__all__ = ["PerformanceEvaluator", "PytestEvaluator"]
 
