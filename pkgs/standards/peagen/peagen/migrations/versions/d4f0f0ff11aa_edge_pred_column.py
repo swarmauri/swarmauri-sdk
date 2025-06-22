f5f61d0531f2c5115c97e88341f54db90e2dcf55
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-22 09:22:49 -0500

    Update d4f0f0ff11aa_edge_pred_column.py

--- pkgs/standards/peagen/peagen/migrations/versions/d4f0f0ff11aa_edge_pred_column.py
+++ pkgs/standards/peagen/peagen/migrations/versions/d4f0f0ff11aa_edge_pred_column.py
@@ -12,6 +12,12 @@ def upgrade() -> None:
     cols = {c["name"] for c in inspector.get_columns("task_runs")}
     if "edge_pred" not in cols:
         op.add_column("task_runs", sa.Column("edge_pred", sa.String()))
+    if "labels" not in cols:
+        op.add_column(
+            "task_runs",
+            sa.Column("labels", sa.JSON(), nullable=False, server_default="[]"),
+        )
+        op.alter_column("task_runs", "labels", server_default=None)
 
 
 def downgrade() -> None:
@@ -20,3 +26,5 @@ def downgrade() -> None:
     cols = {c["name"] for c in inspector.get_columns("task_runs")}
     if "edge_pred" in cols:
         op.drop_column("task_runs", "edge_pred")
+    if "labels" in cols:
+        op.drop_column("task_runs", "labels")
