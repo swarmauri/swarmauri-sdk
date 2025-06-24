6134b6157d24d105f9ebb778dd056fcb18f78ce7
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-23 23:47:29 -0500

    cli: remove remote fetch and expose fetch at root

--- pkgs/standards/peagen/peagen/cli/commands/__init__.py
+++ pkgs/standards/peagen/peagen/cli/commands/__init__.py
@@ -3,7 +3,7 @@
 from .doe import local_doe_app, remote_doe_app
 from .eval import local_eval_app, remote_eval_app
 from .extras import local_extras_app, remote_extras_app
-from .fetch import local_fetch_app, remote_fetch_app
+from .fetch import fetch_app
 from .db import local_db_app, remote_db_app
 from .init import local_init_app, remote_init_app
 from .process import local_process_app, remote_process_app
@@ -31,8 +31,7 @@ __all__ = [
     "remote_evolve_app",
     "local_extras_app",
     "remote_extras_app",
-    "local_fetch_app",
-    "remote_fetch_app",
+    "fetch_app",
     "local_db_app",
     "remote_db_app",
     "local_init_app",
