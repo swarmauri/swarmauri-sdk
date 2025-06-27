0a107c09bfaacb1f0f74abb6a73231517cc63eba
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-27 13:18:20 -0500

    fix schemas naming and task validation

--- pkgs/standards/peagen/peagen/models/task/__init__.py
+++ pkgs/standards/peagen/peagen/models/task/__init__.py
@@ -1,1 +1,21 @@
+from .task import TaskModel, Task
+from .task_run import TaskRunModel, TaskRun
+from .task_relation import TaskRelationModel, TaskRelation
+from .task_run_relation_association import (
+    TaskRunTaskRelationAssociationModel,
+    TaskRunTaskRelationAssociation,
+)
+from .raw_blob import RawBlobModel, RawBlob
 
+__all__ = [
+    "TaskModel",
+    "Task",
+    "TaskRunModel",
+    "TaskRun",
+    "TaskRelationModel",
+    "TaskRelation",
+    "TaskRunTaskRelationAssociationModel",
+    "TaskRunTaskRelationAssociation",
+    "RawBlobModel",
+    "RawBlob",
+]
