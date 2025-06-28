cb96620ffc4834dbcc0be36fac070ab8dd1993a1
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-27 20:52:49 -0500

    fix(peagen): remove aliases

--- pkgs/standards/peagen/peagen/orm/task/__init__.py
+++ pkgs/standards/peagen/peagen/orm/task/__init__.py
@@ -4,21 +4,10 @@ from .task_relation import TaskRelationModel
 from .task_run_relation_association import TaskRunTaskRelationAssociationModel
 from .raw_blob import RawBlobModel
 
-Task = TaskModel
-TaskRun = TaskRunModel
-TaskRelation = TaskRelationModel
-TaskRunTaskRelationAssociation = TaskRunTaskRelationAssociationModel
-RawBlob = RawBlobModel
-
 __all__ = [
     "TaskModel",
-    "Task",
     "TaskRunModel",
-    "TaskRun",
     "TaskRelationModel",
-    "TaskRelation",
     "TaskRunTaskRelationAssociationModel",
-    "TaskRunTaskRelationAssociation",
-    "RawBlobModel",
-    "RawBlob",
+    "RawBlobModel"
 ]
