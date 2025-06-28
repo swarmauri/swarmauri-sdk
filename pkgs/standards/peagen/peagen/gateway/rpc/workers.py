a1f164f6a90405fd5b4ab5059634b505bcbe6909
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-28 00:54:52 -0500

    expose worker and task rpc functions

--- pkgs/standards/peagen/peagen/gateway/rpc/workers.py
+++ pkgs/standards/peagen/peagen/gateway/rpc/workers.py
@@ -17,7 +17,7 @@ from .. import (
     log,
     queue,
 )
-from ..orm.status import Status
+from peagen.orm.status import Status
 from peagen.transport.jsonrpc import RPCException
 from peagen.defaults import (
     WORKER_REGISTER,
