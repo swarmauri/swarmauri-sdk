52b152db548f98203f486aa83c2c0237e4dda40f
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-06-29 16:57:02 -0500

    Update jsonrpc.py

--- pkgs/standards/peagen/peagen/transport/jsonrpc.py
+++ pkgs/standards/peagen/peagen/transport/jsonrpc.py
@@ -4,9 +4,8 @@ import inspect
 from typing import Callable, Dict
 
 from pydantic import BaseModel
-from peagen.protocols import _registry
-
-from peagen.protocols import Error
+from . import _registry
+from .error_codes import Error
 
 
 class RPCException(Exception):
