ef4e5c8d1caf4f78267ebd41b962ca6aa278383a
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-08-08 16:35:55 -0500

    fix: debug

--- pkgs/standards/auto_authn/auto_authn/v2/orm/tables.py
+++ pkgs/standards/auto_authn/auto_authn/v2/orm/tables.py
@@ -107,7 +107,7 @@ class Client(ClientBase):  # Tenant FK via mix-in
 # --------------------------------------------------------------------
 class User(UserBase):
     """Human principal with authentication credentials."""
-    __table_args__ = {"extend_existing": True, "schema": "authn"}
+    __table_args__ = {"schema": "authn"}
     email = Column(String(120), nullable=False, unique=True)
     password_hash = Column(LargeBinary(60))
     api_keys = relationship(
