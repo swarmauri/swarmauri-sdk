7345192d2e2bba360f518dd79e9b693be055acf8
MichaelDecent <michaeldecent2@gmail.com>
2025-08-20 13:46:48 +0100

    Update key.py to import b64d_optional for enhanced base64 decoding in encrypt and decrypt methods

--- pkgs/standards/auto_kms/auto_kms/tables/key.py
+++ pkgs/standards/auto_kms/auto_kms/tables/key.py
@@ -222,7 +222,7 @@ class Key(Base):
     )
     async def encrypt(cls, ctx):
         import base64
-        from ..utils import b64d
+        from ..utils import b64d, b64d_optional
 
         p = ctx.get("payload") or {}
         crypto = getattr(
@@ -318,6 +318,7 @@ class Key(Base):
     )
     async def decrypt(cls, ctx):
         import base64
+        from ..utils import b64d, b64d_optional
 
         p = ctx.get("payload") or {}
         crypto = getattr(
