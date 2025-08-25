aa58d5c97f1c4f4d6c695b4db90b36adf4513ac0
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-08-25 12:21:07 -0500

    feat(auto_authn): add rs256 jwa support

--- pkgs/standards/auto_authn/tests/unit/test_rfc7518_jwa.py
+++ pkgs/standards/auto_authn/tests/unit/test_rfc7518_jwa.py
@@ -5,3 +5,7 @@ from auto_authn.v2 import supported_algorithms
 
 def test_supported_algorithms_contains_eddsa() -> None:
     assert "EdDSA" in supported_algorithms()
+
+
+def test_supported_algorithms_contains_rs256() -> None:
+    assert "RS256" in supported_algorithms()
