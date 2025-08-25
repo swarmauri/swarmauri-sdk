5940d5c54e05fe4d4a9cf1fb8429fe890035f1ce
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-08-25 08:54:37 -0500

    Use internal InvalidTokenError in RFC7521 tests

--- pkgs/standards/auto_authn/tests/unit/test_rfc7521_assertion_framework.py
+++ pkgs/standards/auto_authn/tests/unit/test_rfc7521_assertion_framework.py
@@ -7,7 +7,7 @@ import time
 from unittest.mock import patch
 
 import pytest
-from jwt.exceptions import InvalidTokenError
+from auto_authn.v2.errors import InvalidTokenError
 
 from auto_authn.v2 import encode_jwt
 from auto_authn.v2.rfc7521 import (
