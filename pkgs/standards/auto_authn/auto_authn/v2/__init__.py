d82cd88ac1b0390a7764e28939f4b5123f0aa3c6
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-08-25 06:25:11 -0500

    feat: add RFC 8628 compliance helpers and tests

--- pkgs/standards/auto_authn/auto_authn/v2/__init__.py
+++ pkgs/standards/auto_authn/auto_authn/v2/__init__.py
@@ -9,7 +9,12 @@ from .rfc7636_pkce import (
     create_code_verifier,
     verify_code_challenge,
 )
-from .rfc8628 import generate_device_code, generate_user_code, validate_user_code
+from .rfc8628 import (
+    generate_device_code,
+    generate_user_code,
+    validate_user_code,
+    RFC8628_SPEC_URL,
+)
 from .rfc9396 import AuthorizationDetail, parse_authorization_details
 from .rfc6750 import extract_bearer_token
 from .rfc7662 import introspect_token, register_token, reset_tokens
@@ -18,6 +23,7 @@ from .rfc9126 import store_par_request, get_par_request, reset_par_store
 from .rfc8707 import extract_resource, RFC8707_SPEC_URL
 from .rfc8705 import thumbprint_from_cert_pem, validate_certificate_binding
 from .rfc8252 import is_native_redirect_uri, validate_native_redirect_uri
+
 __all__ = [
     "create_code_verifier",
     "create_code_challenge",
@@ -25,6 +31,7 @@ __all__ = [
     "generate_user_code",
     "validate_user_code",
     "generate_device_code",
+    "RFC8628_SPEC_URL",
     "parse_authorization_details",
     "AuthorizationDetail",
     "extract_bearer_token",
