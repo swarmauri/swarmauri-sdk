04ee758db613174e488b50b03a9d0d1f74912ee5
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-08-27 10:21:08 -0500

    chore: rm lcase export

--- pkgs/standards/autoapi/autoapi/v3/deps/__init__.py
+++ pkgs/standards/autoapi/autoapi/v3/deps/__init__.py
@@ -14,7 +14,6 @@ from .pydantic import *  # noqa: F403, F401
 
 # Re-export all FastAPI dependencies
 from .fastapi import *  # noqa: F403, F401
-from .fastapi import App as app  # noqa: F401
 
 # Note: starlette.py is reserved for future Starlette-specific imports
 # if we need to import directly from Starlette rather than through FastAPI
