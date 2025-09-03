d5ed8ab44407ebd41da1489687162e99acbfbf96
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-09-03 03:08:22 -0500

    chore(authnz): rename title

--- pkgs/standards/auto_authn/auto_authn/app.py
+++ pkgs/standards/auto_authn/auto_authn/app.py
@@ -32,7 +32,7 @@ from .rfc7009 import include_rfc7009
 # AutoApp application
 # --------------------------------------------------------------------
 app = AutoApp(
-    title="AutoAPI-AuthN",
+    title="Auto-AuthNZ",
     version="0.1.0",
     openapi_url="/openapi.json",
     docs_url="/docs",
