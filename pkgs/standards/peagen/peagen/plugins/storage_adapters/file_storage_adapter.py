dbc183cacaa7aecdfb265aeb4a1aae6739ba869c
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-09-03 06:15:45 -0500

    fix(peagne): rm internal plugin ref, defer to pg mnger

--- pkgs/standards/peagen/peagen/plugins/storage_adapters/file_storage_adapter.py
+++
@@ -1,5 +0,0 @@
-"""Wrapper to expose the FileStorageAdapter as a built-in plugin."""
-
-from swarmauri_storage_file.file_storage_adapter import FileStorageAdapter
-
-__all__ = ["FileStorageAdapter"]
