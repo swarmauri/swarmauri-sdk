88d41f1561a93e392ed5d52fea552d2182e95184
MichaelDecent <michaeldecent2@gmail.com>
2025-09-05 17:12:16 +0100

    feat(llms): enhance allowed models across multiple LLM classes for improved functionality and compatibility

--- pkgs/swarmauri_standard/swarmauri_standard/tool_llms/AnthropicToolModel.py
+++ pkgs/swarmauri_standard/swarmauri_standard/tool_llms/AnthropicToolModel.py
@@ -55,16 +55,13 @@ class AnthropicToolModel(ToolLLMBase):
     _async_client: httpx.AsyncClient = PrivateAttr()
     api_key: SecretStr
     allowed_models: List[str] = [
+        "claude-opus-4-1",
+        "claude-opus-4-0",
+        "claude-sonnet-4-0",
         "claude-3-7-sonnet-latest",
         "claude-3-5-haiku-latest",
-        "claude-3-5-sonnet-latest",
-        "claude-3-opus-latest",
-        "claude-3-5-sonnet-20241022",
         "claude-3-5-haiku-20241022",
-        "claude-3-7-sonnet-20250219,claude-3-5-sonnet-20240620",
-        "claude-3-opus-20240229",
-        "claude-3-sonnet-20240229",
-        "claude-3-haiku-20240307",
+        "claude-3-7-sonnet-20250219",
     ]
     name: str = "claude-3-sonnet-20240229"
 
--- pkgs/swarmauri_standard/swarmauri_standard/tool_llms/CohereToolModel.py
+++ pkgs/swarmauri_standard/swarmauri_standard/tool_llms/CohereToolModel.py
@@ -55,7 +55,13 @@ class CohereToolModel(ToolLLMBase):
     allowed_models: List[str] = [
         "command-a-03-2025",
         "command-r7b-12-2024",
+        "command-a-translate-08-2025",
+        "command-a-reasoning-08-2025",
+        "command-a-vision-07-2025",
+        "command-r-plus-04-2024",
         "command-r-plus",
+        "command-r-08-2024",
+        "command-r-03-2024",
         "command-r",
         "command",
         "command-nightly",
