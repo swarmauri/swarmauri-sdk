dea96d523330c1e2722c2c9cc9200e006010ff9b
cobycloud <25079070+cobycloud@users.noreply.github.com>
2025-09-05 17:28:33 -0500

    chore(autoapi): stop exporting collect helpers

--- pkgs/standards/autoapi/autoapi/v3/decorators.py
+++ pkgs/standards/autoapi/autoapi/v3/decorators.py
@@ -6,8 +6,6 @@ from .op.decorators import *  # noqa: F401,F403
 from .hook.decorators import *  # noqa: F401,F403
 from .schema.decorators import schema_ctx
 from .engine.decorators import engine_ctx
-from .op.collect import collect_decorated_ops, alias_map_for, apply_alias
-from .hook.collect import collect_decorated_hooks
 from .op.decorators import __all__ as _op_all
 from .hook.decorators import __all__ as _hook_all
 
@@ -16,8 +14,4 @@ __all__ = [
     *_hook_all,
     "schema_ctx",
     "engine_ctx",
-    "collect_decorated_ops",
-    "collect_decorated_hooks",
-    "alias_map_for",
-    "apply_alias",
 ]
