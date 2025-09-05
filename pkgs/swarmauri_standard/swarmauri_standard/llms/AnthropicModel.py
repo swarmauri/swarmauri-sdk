39e9ee9657989a47fe92efe380327b5d751991d4
MichaelDecent <michaeldecent2@gmail.com>
2025-09-05 09:52:15 +0100

    feat(llms): update allowed models for AI21StudioModel and AnthropicModel

--- pkgs/swarmauri_standard/swarmauri_standard/llms/AnthropicModel.py
+++ pkgs/swarmauri_standard/swarmauri_standard/llms/AnthropicModel.py
@@ -34,17 +34,13 @@ class AnthropicModel(LLMBase):
     _async_client: httpx.AsyncClient = PrivateAttr()
     api_key: SecretStr
     allowed_models: List[str] = [
+        "claude-opus-4-1",
+        "claude-opus-4-0",
+        "claude-sonnet-4-0",
         "claude-3-7-sonnet-latest",
         "claude-3-5-haiku-latest",
-        "claude-3-5-sonnet-latest",
-        "claude-3-opus-latest",
-        "claude-3-5-sonnet-20241022",
         "claude-3-5-haiku-20241022",
         "claude-3-7-sonnet-20250219",
-        "claude-3-5-sonnet-20240620",
-        "claude-3-opus-20240229",
-        "claude-3-sonnet-20240229",
-        "claude-3-haiku-20240307",
     ]
     name: str = "claude-3-7-sonnet-latest"
     type: Literal["AnthropicModel"] = "AnthropicModel"
