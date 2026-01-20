3d84607e7c55eb372ba839854a69fd85fb8e43cc
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-01-20 12:31:34 -0600

    Fix DPoP verifier header parsing

--- pkgs/base/swarmauri_base/pop/__init__.py
+++ pkgs/base/swarmauri_base/pop/__init__.py
@@ -1,7 +1,7 @@
 """Base helpers for proof-of-possession implementations."""
 
 from .PopSignerMixin import PopSignerMixin, RequestContext
-from .PopSigningBase import PopSigningBase, PopSignerBase
+from .PopSigningBase import PopSigningBase
 from .PopVerifierMixin import PopVerifierBase, PopVerifierMixin
 from .binding import make_cnf_cose, make_cnf_jkt, make_cnf_x5t, normalize_cnf
 from .util import canon_htm_htu, sha256_b64u
@@ -9,7 +9,6 @@ from .util import canon_htm_htu, sha256_b64u
 __all__ = [
     "PopSignerMixin",
     "PopSigningBase",
-    "PopSignerBase",
     "PopVerifierMixin",
     "PopVerifierBase",
     "RequestContext",
