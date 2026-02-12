471959a9fcfbe14710c534cdb060fae8c488590c
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-02-12 17:27:39 -0600

    Update tigrbl-lens embed to v0.0.12

--- pkgs/standards/tigrbl/tigrbl/system/docs/lens.py
+++ pkgs/standards/tigrbl/tigrbl/system/docs/lens.py
@@ -6,7 +6,7 @@ from urllib.parse import quote
 from ...response.stdapi import Response
 
 
-TIGRBL_LENS_VERSION = "0.0.11"
+TIGRBL_LENS_VERSION = "0.0.12"
 
 
 def _with_leading_slash(path: str) -> str:
