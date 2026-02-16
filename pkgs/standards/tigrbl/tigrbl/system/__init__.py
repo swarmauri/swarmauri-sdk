98fed08bbadd43c88938568bd742adad49258b3c
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-02-16 04:40:54 -0600

    Refine tigrbl system helper grouping and favicon naming

--- pkgs/standards/tigrbl/tigrbl/system/__init__.py
+++ pkgs/standards/tigrbl/tigrbl/system/__init__.py
@@ -15,24 +15,6 @@ def mount_diagnostics(*args: Any, **kwargs: Any) -> Any:
     return _mount_diagnostics(*args, **kwargs)
 
 
-def build_openapi(*args: Any, **kwargs: Any) -> Any:
-    from .docs import build_openapi as _build_openapi
-
-    return _build_openapi(*args, **kwargs)
-
-
-def build_swagger(*args: Any, **kwargs: Any) -> Any:
-    from .docs import build_swagger as _build_swagger
-
-    return _build_swagger(*args, **kwargs)
-
-
-def build_lens(*args: Any, **kwargs: Any) -> Any:
-    from .docs import build_lens as _build_lens
-
-    return _build_lens(*args, **kwargs)
-
-
 def mount_openapi(*args: Any, **kwargs: Any) -> Any:
     from .docs import mount_openapi as _mount_openapi
 
@@ -57,22 +39,40 @@ def mount_openrpc(*args: Any, **kwargs: Any) -> Any:
     return _mount_openrpc(*args, **kwargs)
 
 
-def build_openrpc_spec(*args: Any, **kwargs: Any) -> Any:
-    from .docs import build_openrpc_spec as _build_openrpc_spec
+def mount_favicon(*args: Any, **kwargs: Any) -> Any:
+    from .favicon import mount_favicon as _mount_favicon
 
-    return _build_openrpc_spec(*args, **kwargs)
+    return _mount_favicon(*args, **kwargs)
 
 
-def favicon_endpoint(*args: Any, **kwargs: Any) -> Any:
+def build_openapi(*args: Any, **kwargs: Any) -> Any:
+    from .docs import build_openapi as _build_openapi
+
+    return _build_openapi(*args, **kwargs)
+
+
+def build_swagger(*args: Any, **kwargs: Any) -> Any:
+    from .docs import build_swagger as _build_swagger
+
+    return _build_swagger(*args, **kwargs)
+
+
+def build_lens(*args: Any, **kwargs: Any) -> Any:
+    from .docs import build_lens as _build_lens
+
+    return _build_lens(*args, **kwargs)
+
+
+def build_favicon(*args: Any, **kwargs: Any) -> Any:
     from .favicon import favicon_endpoint as _favicon_endpoint
 
     return _favicon_endpoint(*args, **kwargs)
 
 
-def mount_favicon(*args: Any, **kwargs: Any) -> Any:
-    from .favicon import mount_favicon as _mount_favicon
+def build_openrpc_spec(*args: Any, **kwargs: Any) -> Any:
+    from .docs import build_openrpc_spec as _build_openrpc_spec
 
-    return _mount_favicon(*args, **kwargs)
+    return _build_openrpc_spec(*args, **kwargs)
 
 
 def stop_uvicorn_server(*args: Any, **kwargs: Any) -> Any:
@@ -82,17 +82,17 @@ def stop_uvicorn_server(*args: Any, **kwargs: Any) -> Any:
 
 
 __all__ = [
-    "build_lens",
-    "build_openapi",
-    "build_openrpc_spec",
-    "build_swagger",
     "FAVICON_PATH",
-    "favicon_endpoint",
     "mount_diagnostics",
     "mount_favicon",
     "mount_lens",
     "mount_openapi",
     "mount_openrpc",
     "mount_swagger",
+    "build_favicon",
+    "build_lens",
+    "build_openapi",
+    "build_openrpc_spec",
+    "build_swagger",
     "stop_uvicorn_server",
 ]
