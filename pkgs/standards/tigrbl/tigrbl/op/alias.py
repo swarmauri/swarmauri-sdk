d7e9595c3b703c3dcbc1d814bca2efdc82c08766
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-02-22 23:46:26 -0600

    Fix tigrbl alias helper abstract Alias instantiation

--- pkgs/standards/tigrbl/tigrbl/op/alias.py
+++ pkgs/standards/tigrbl/tigrbl/op/alias.py
@@ -10,9 +10,33 @@ from ..schema.types import SchemaArg
 
 @dataclass(frozen=True)
 class Alias(AliasSpec):
-    alias: str
-    request_schema: Optional[SchemaArg] = None
-    response_schema: Optional[SchemaArg] = None
-    persist: Optional[PersistPolicy] = None
-    arity: Optional[Arity] = None
-    rest: Optional[bool] = None
+    _alias: str
+    _request_schema: Optional[SchemaArg] = None
+    _response_schema: Optional[SchemaArg] = None
+    _persist: Optional[PersistPolicy] = None
+    _arity: Optional[Arity] = None
+    _rest: Optional[bool] = None
+
+    @property
+    def alias(self) -> str:
+        return self._alias
+
+    @property
+    def request_schema(self) -> Optional[SchemaArg]:
+        return self._request_schema
+
+    @property
+    def response_schema(self) -> Optional[SchemaArg]:
+        return self._response_schema
+
+    @property
+    def persist(self) -> Optional[PersistPolicy]:
+        return self._persist
+
+    @property
+    def arity(self) -> Optional[Arity]:
+        return self._arity
+
+    @property
+    def rest(self) -> Optional[bool]:
+        return self._rest
