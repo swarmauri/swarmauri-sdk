c48d4a39e88432e68f4327c2206d71c31161c8d4
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-03-01 04:58:14 -0600

    tigrbl: use concrete request/response imports

--- pkgs/standards/tigrbl/tigrbl/runtime/atoms/response/templates.py
+++ pkgs/standards/tigrbl/tigrbl/runtime/atoms/response/templates.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 import logging
 from typing import Any, Dict, Iterable, Optional, Tuple
 
-from ....requests import Request
+from ...._concrete._request import Request
 from ....vendor.jinja import (
     Environment,
     FileSystemLoader,
