d676321d7f59b26b545a16265157d5c2de019ddb
MichaelDecent <michaeldecent2@gmail.com>
2026-03-13 16:28:58 +0100

    change _Ctx to inherit from dict

--- pkgs/standards/tigrbl/tigrbl/types/__init__.py
+++ pkgs/standards/tigrbl/tigrbl/types/__init__.py
@@ -1,6 +1,7 @@
 """Compatibility type exports for legacy ``tigrbl.types`` imports."""
 
 from tigrbl.column import F, IO, S, acol
+from tigrbl_typing.types import *  # noqa: F403
 from tigrbl_typing.types import __all__ as _typing_all
 
 __all__ = [*_typing_all, "F", "IO", "S", "acol"]
