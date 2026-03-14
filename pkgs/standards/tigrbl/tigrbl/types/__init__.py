99ee4c6f7b7a1d34ef9df5c4a747506e99dbc83b
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-03-14 14:08:11 -0500

    Remove AllowAnonProvider backward compatibility exports

--- pkgs/standards/tigrbl/tigrbl/types/__init__.py
+++ pkgs/standards/tigrbl/tigrbl/types/__init__.py
@@ -3,8 +3,9 @@
 from tigrbl.column import F, IO, S, acol
 from tigrbl_typing import types as _typing_types
 from tigrbl_typing.types import __all__ as _typing_all
+from tigrbl_concrete.decorators import allow_anon
 
 for _name in _typing_all:
     globals()[_name] = getattr(_typing_types, _name)
 
-__all__ = [*_typing_all, "F", "IO", "S", "acol"]
+__all__ = [*_typing_all, "F", "IO", "S", "acol", "allow_anon"]
