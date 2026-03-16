11e4ffb0e4632e5e6110226fc826c0b25ba99e91
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-03-16 03:55:16 -0500

    Fix tigrbl canon rest helper underscore exports

--- pkgs/standards/tigrbl_canon/tigrbl_canon/mapping/rest/helpers.py
+++ pkgs/standards/tigrbl_canon/tigrbl_canon/mapping/rest/helpers.py
@@ -2,10 +2,28 @@ from __future__ import annotations
 
 from warnings import warn
 
-from tigrbl_concrete._mapping.rest.helpers import *  # noqa: F403
+from tigrbl_concrete._mapping.rest.helpers import (
+    _Key,
+    _coerce_parent_kw,
+    _ensure_jsonable,
+    _get_phase_chains,
+    _pk_name,
+    _pk_names,
+    _req_state_db,
+)
 
 warn(
     "tigrbl_canon.mapping.rest.helpers is deprecated; use tigrbl_concrete._mapping.rest.helpers",
     DeprecationWarning,
     stacklevel=2,
 )
+
+__all__ = [
+    "_Key",
+    "_coerce_parent_kw",
+    "_ensure_jsonable",
+    "_get_phase_chains",
+    "_pk_name",
+    "_pk_names",
+    "_req_state_db",
+]
