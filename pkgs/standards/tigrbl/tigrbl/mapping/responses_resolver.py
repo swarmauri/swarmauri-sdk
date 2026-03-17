30fba07f23831e952aadfd71c3eae0d3402cf5f8
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-03-17 01:56:31 -0500

    remove legacy tigrbl mapping facade module

--- pkgs/standards/tigrbl/tigrbl/mapping/responses_resolver.py
+++
@@ -1,5 +0,0 @@
-"""Response resolver compatibility entrypoint for ``tigrbl.mapping``."""
-
-from tigrbl_core._spec.response_resolver import resolve_response_spec
-
-__all__ = ["resolve_response_spec"]
